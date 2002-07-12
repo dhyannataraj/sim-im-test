d9add4aab5b882f0523b4161abd0c1e790b08f21
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2002-07-12 00:03:59 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.2'.
    git-svn-id: file:///.data/svn_convert/svn@20 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.4.2/sim/mainwin.h
@@ -0,0 +1,317 @@
+/***************************************************************************
+                          mainwin.h  -  description
+                             -------------------
+    begin                : Sun Mar 10 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : shutoff@mail.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _MAINWIN_H
+#define _MAINWIN_H
+
+#include "defs.h"
+
+#include <fstream>
+
+#include <qmainwindow.h>
+#include <qtimer.h>
+
+#include "cfg.h"
+
+class Skin;
+class DockWnd;
+class KPopupMenu;
+class UserView;
+class CToolButton;
+class PictButton;
+class QTimer;
+class UserFloat;
+class UserBox;
+class QPopupMenu;
+class QToolBar;
+class Themes;
+class SearchDialog;
+class SetupDialog;
+class ICQMessage;
+class ICQUser;
+class TransparentTop;
+
+const int mnuAction = 0;
+const int mnuMessage = 1;
+const int mnuURL = 2;
+const int mnuSMS = 3;
+const int mnuFile = 4;
+const int mnuContacts = 5;
+const int mnuAuth = 6;
+const int mnuChat = 7;
+const int mnuFloating = 10;
+const int mnuClose = 11;
+const int mnuInfo = 12;
+const int mnuHistory = 13;
+const int mnuSearch = 14;
+const int mnuDelete = 15;
+const int mnuGroups = 16;
+const int mnuMail = 17;
+const int mnuContainers = 18;
+const int mnuActionInt = 19;
+const int mnuGrpCreate = 21;
+const int mnuGrpCollapseAll = 22;
+const int mnuGrpExpandAll = 23;
+const int mnuGrpRename = 24;
+const int mnuGrpDelete = 25;
+const int mnuGrpUp = 26;
+const int mnuGrpDown = 27;
+const int mnuAbout = 30;
+const int mnuAboutKDE = 31;
+const int mnuGrpTitle = 0x10000;
+
+const unsigned long mnuGroupVisible = 0x10001;
+const unsigned long mnuGroupInvisible = 0x10002;
+const unsigned long mnuGroupIgnore = 0x10003;
+const unsigned long mnuOnTop = 0x10004;
+
+const unsigned short ContainerModeUser = 0;
+const unsigned short ContainerModeGroup = 1;
+const unsigned short ContainerModeAll = 2;
+
+const unsigned long ContainerAllUsers = 0xFFFFFFFF;
+
+class XOSD;
+class ICQEvent;
+class QDialog;
+class KAboutKDE;
+
+class MainWindow : public QMainWindow, public ConfigArray
+{
+    Q_OBJECT
+public:
+    MainWindow(const char *name = NULL);
+    ~MainWindow();
+
+    ConfigBool   Show;
+    ConfigBool	 OnTop;
+
+    ConfigBool   ShowOffline;
+    ConfigBool   GroupMode;
+
+    ConfigShort  mLeft;
+    ConfigShort  mTop;
+    ConfigShort  mWidth;
+    ConfigShort  mHeight;
+
+    ConfigString UseStyle;
+
+    ConfigULong  AutoAwayTime;
+    ConfigULong  AutoNATime;
+
+    ConfigULong  ManualStatus;
+    ConfigShort  DivPos;
+
+    ConfigBool   SpellOnSend;
+
+    ConfigString ToolbarDock;
+    ConfigShort	 ToolbarOffset;
+    ConfigShort  ToolbarY;
+
+    ConfigString IncomingMessage;
+    ConfigString IncomingURL;
+    ConfigString IncomingSMS;
+    ConfigString IncomingAuth;
+    ConfigString IncomingFile;
+    ConfigString IncomingChat;
+    ConfigString FileDone;
+    ConfigString OnlineAlert;
+    ConfigString BirthdayReminder;
+
+    ConfigString UrlViewer;
+    ConfigString MailClient;
+    ConfigString SoundPlayer;
+
+    ConfigBool   UseTransparent;
+    ConfigULong  Transparent;
+    ConfigBool	 UseTransparentContainer;
+    ConfigULong	 TransparentContainer;
+
+    ConfigBool	 NoShowAway;
+    ConfigBool	 NoShowNA;
+    ConfigBool	 NoShowOccupied;
+    ConfigBool	 NoShowDND;
+    ConfigBool	 NoShowFFC;
+
+    ConfigBool	 UseSystemFonts;
+
+    ConfigString FontFamily;
+    ConfigUShort FontSize;
+    ConfigUShort FontWeight;
+    ConfigBool   FontItalic;
+
+    ConfigString FontMenuFamily;
+    ConfigUShort FontMenuSize;
+    ConfigUShort FontMenuWeight;
+    ConfigBool   FontMenuItalic;
+
+    ConfigULong	 ColorSend;
+    ConfigULong	 ColorReceive;
+
+    ConfigShort	 ChatWidth;
+    ConfigShort  ChatHeight;
+
+    ConfigBool	 CloseAfterSend;
+    ConfigBool	 UserWindowInTaskManager;
+
+    ConfigString Icons;
+
+    ConfigBool   XOSD_on;
+    ConfigShort  XOSD_pos;
+    ConfigShort  XOSD_offset;
+    ConfigULong  XOSD_color;
+    ConfigString XOSD_FontFamily;
+    ConfigUShort XOSD_FontSize;
+    ConfigUShort XOSD_FontWeight;
+    ConfigBool   XOSD_FontItalic;
+    ConfigUShort XOSD_timeout;
+
+    ConfigUShort ContainerMode;
+
+    bool 	     init();
+
+    QPopupMenu   *menuStatus;
+    QPopupMenu	 *menuPhone;
+    QPopupMenu	 *menuPhoneLocation;
+    QPopupMenu	 *menuPhoneStatus;
+    KPopupMenu   *menuFunction;
+    KPopupMenu   *menuUser;
+    QPopupMenu	 *menuGroup;
+    QPopupMenu   *menuContainers;
+    QPopupMenu   *menuGroups;
+
+    UserView     *users;
+
+    CToolButton  *btnShowOffline;
+    CToolButton  *btnGroupMode;
+    PictButton   *btnStatus;
+    Themes *themes;
+    XOSD	*xosd;
+
+    void setShow(bool bState);
+    bool isShow();
+    void buildFileName(string &s, const char *name);
+    void playSound(const char *wav);
+
+    string homeDir;
+
+    void adjustUserMenu(QPopupMenu *menu, ICQUser *u, bool bHaveTitle);
+    void adjustGroupMenu(QPopupMenu *menu, unsigned long uin);
+    void destroyBox(UserBox*);
+    unsigned long m_uin;
+    QRect m_rc;
+
+    void setFonts();
+    void changeColors();
+    void changeWm();
+
+signals:
+    void transparentChanged();
+    void colorsChanged();
+    void setupInit();
+    void iconChanged();
+    void wmChanged();
+public slots:
+    void quit();
+    void setup();
+    void phonebook();
+    void search();
+    void toggleShow();
+    void showPopup(QPoint);
+    void saveState();
+    void toggleGroupMode();
+    void toggleShowOffline();
+    void setGroupMode(bool);
+    void setShowOffline(bool);
+    void setStatus(int);
+    void showUserPopup(unsigned long uin, QPoint, QPopupMenu*, const QRect&);
+    void userFunction(int);
+    void userFunction(unsigned long uin, int, unsigned long param=0);
+    void goURL(const char*);
+    void sendMail(unsigned long);
+    void toggleOnTop();
+    void moveUser(int);
+    void changeTransparent();
+    void changeIcons(int);
+protected slots:
+    void realSetStatus();
+    void autoAway();
+    void setToggle();
+    void blink();
+    void processEvent(ICQEvent*);
+    void messageReceived(ICQMessage *msg);
+    void dockDblClicked();
+    void setPhoneLocation(int);
+    void setPhoneStatus(int);
+    void toContainer(int);
+    void addNonIM();
+    void showGroupPopup(QPoint);
+    void deleteUser(int);
+    void ignoreUser(int);
+    void about();
+    void about_kde();
+    void dialogFinished();
+    void timerExpired();
+protected:
+    char realTZ;
+
+    int lockFile;
+
+    bool bBlinkState;
+
+    list<UserFloat*> floating;
+    UserFloat *findFloating(unsigned long uin, bool bDelete = false);
+
+    bool m_bAutoAway, m_bAutoNA;
+    int  m_autoStatus;
+    bool noToggle;
+    QTimer *autoAwayTimer;
+    QTimer *blinkTimer;
+    void setIcons();
+    void setDock(bool bState);
+    unsigned long uinMenu;
+    void setOnTop();
+    void ownerChanged();
+    void saveContacts();
+    void setStatusIcon(QPixmap *p);
+    void addStatusItem(int status);
+    void setStatusItem(int status);
+    bool	bQuit;
+    DockWnd	*dock;
+    virtual void closeEvent(QCloseEvent*);
+    void addMessageType(QPopupMenu *menu, int type, int id, bool bAdd, bool bHaveTitle);
+    QToolBar *toolbar;
+    list<UserBox*> containers;
+    SearchDialog *searchDlg;
+    SetupDialog *setupDlg;
+    bool bInLogin;
+
+    void loadMenu();
+    void showUser(unsigned long uin, int function, unsigned long param=0);
+    void closeUser(unsigned long uin);
+
+    TransparentTop *transparent;
+#if USE_KDE
+    QDialog      *mAboutApp;
+    KAboutKDE    *mAboutKDE;
+#endif
+};
+
+extern MainWindow *pMain;
+
+#endif
+
---
+++ branches/unlabeled-1.4.2/sim/msgview.cpp
@@ -0,0 +1,484 @@
+/***************************************************************************
+                          msgview.cpp  -  description
+                             -------------------
+    begin                : Sat Mar 16 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : shutoff@mail.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "msgview.h"
+#include "mainwin.h"
+#include "client.h"
+#include "cuser.h"
+#include "history.h"
+#include "log.h"
+#include "icons.h"
+#include "transparent.h"
+
+#if USE_KDE
+#include <kglobal.h>
+#if HAVE_KROOTPIXMAP_H
+#include <krootpixmap.h>
+#endif
+#endif
+
+#include <qdatetime.h>
+#include <qpopupmenu.h>
+#include <qapplication.h>
+#include <qclipboard.h>
+#include <qpainter.h>
+#include <qregexp.h>
+
+#if _MSC_VER > 1020
+#pragma warning(disable:4786)
+#endif
+
+QString ParseText(const char *text);
+
+TextShow::TextShow(QWidget *p)
+        : QTextBrowser(p)
+{
+    bg = new TransparentBg(this);
+    baseBG = colorGroup().color(QColorGroup::Base);
+    baseFG = colorGroup().color(QColorGroup::Text);
+    setTextFormat(RichText);
+    menu = NULL;
+}
+
+void TextShow::resizeEvent(QResizeEvent *e)
+{
+    QTextBrowser::resizeEvent(e);
+    scrollToAnchor(curAnchor);
+}
+
+void TextShow::viewportMousePressEvent(QMouseEvent *e)
+{
+    if ((e->button() == RightButton) && hasSelectedText()){
+        if (menu == NULL){
+            menu = new QPopupMenu(this);
+            menu->insertItem(Icon("editcopy"), i18n("&Copy"), this, SLOT(copy()), 0, 1);
+        }
+        menu->popup(e->globalPos());
+        return;
+    }
+    QTextBrowser::viewportMousePressEvent(e);
+}
+
+void TextShow::copy()
+{
+    if (!hasSelectedText()) return;
+    QString msgText = QString::fromLocal8Bit(pClient->clearHTML(selectedText().local8Bit()).c_str());
+    QApplication::clipboard()->setText(msgText);
+}
+
+void TextShow::resetColors()
+{
+    setBackground(baseBG);
+    setForeground(baseFG);
+    bg->setTransparent(true);
+}
+
+void TextShow::setBackground(const QColor& c)
+{
+    QPalette pal = palette();
+    pal.setColor(QPalette::Active, QColorGroup::Base, c);
+    pal.setColor(QPalette::Inactive, QColorGroup::Base, c);
+    pal.setColor(QPalette::Disabled, QColorGroup::Base, c);
+    setPalette(pal);
+    bg->setTransparent(c == baseBG);
+}
+
+void TextShow::setForeground(const QColor& c)
+{
+    QPalette pal = palette();
+
+    pal.setColor(QPalette::Active, QColorGroup::Text, c);
+    pal.setColor(QPalette::Inactive, QColorGroup::Text, c);
+
+    setPalette(pal);
+}
+
+void TextShow::setSource(const QString &href)
+{
+    QString l = href.left(6);
+    if (l == "msg://"){
+        QString id = href.mid(6);
+        int p = id.find('/');
+        if (p >= 0) id = id.left(p);
+        QStringList s = QStringList::split('.', id);
+        if (s.count() >= 2)
+            emit goMessage(s[0].toULong(), s[1].toULong());
+        return;
+    }
+    pMain->goURL(href);
+    return;
+}
+
+QString TextShow::makeMessageText(ICQMessage *msg)
+{
+    QString s;
+    switch (msg->Type()){
+    case ICQ_MSGxMSG:
+        s += ParseText((static_cast<ICQMsg*>(msg))->Message);
+        break;
+    case ICQ_MSGxURL:{
+            ICQUrl *url = static_cast<ICQUrl*>(msg);
+            s += quoteText(url->URL);
+            if (*url->Message.c_str()){
+                s += "<br>";
+                s += ParseText(url->Message);
+            }
+            break;
+        }
+    case ICQ_MSGxAUTHxREQUEST:{
+            ICQAuthRequest *req = static_cast<ICQAuthRequest*>(msg);
+            s += i18n("Authorization request");
+            if (*req->Message.c_str()){
+                s += "<br>";
+                s += ParseText(req->Message);
+            }
+            break;
+        }
+    case ICQ_MSGxAUTHxREFUSED:{
+            ICQAuthRefused *req = static_cast<ICQAuthRefused*>(msg);
+            s += i18n("Authorization refused");
+            if (*req->Message.c_str()){
+                s += "<br>";
+                s += ParseText(req->Message);
+            }
+            break;
+        }
+    case ICQ_MSGxAUTHxGRANTED:
+        s += i18n("Authorization granted");
+        break;
+    case ICQ_MSGxADDEDxTOxLIST:
+        s += i18n("Added to contact list");
+        break;
+    case ICQ_MSGxCONTACTxREQUEST:{
+            ICQContactRequest *req = static_cast<ICQContactRequest*>(msg);
+            s += i18n("Contact request");
+            if (*req->Message.c_str()){
+                s += "<br>";
+                s += ParseText(req->Message);
+            }
+            break;
+        }
+    case ICQ_MSGxFILE:{
+            ICQFile *file = static_cast<ICQFile*>(msg);
+            s += i18n("File: ");
+            s += file->Name.c_str();
+            s += " (";
+            s += QString::number(file->Size);
+            s += " ";
+            s += i18n("bytes");
+            s += ")";
+            s += "<br>";
+            s += ParseText(file->Description);
+            break;
+        }
+    case ICQ_MSGxCHAT:{
+            ICQChat *chat = static_cast<ICQChat*>(msg);
+            s += ParseText(chat->Reason);
+            break;
+        }
+    case ICQ_MSGxCONTACTxLIST:{
+            ICQContacts *contacts = static_cast<ICQContacts*>(msg);
+            s += msg->Received ? i18n("Contact list received") : i18n("Contact list sent");
+            s += "<br>";
+            s += "<table>";
+            for (ContactList::iterator it = contacts->Contacts.begin(); it != contacts->Contacts.end(); it++){
+                Contact *contact = static_cast<Contact*>(*it);
+                s += "<tr><td align=right>" + QString::number(contact->Uin) + "</td><td>";
+                s += quoteText(contact->Alias.c_str());
+                s += "</td></tr>";
+            }
+            s += "</table>";
+            break;
+        }
+    case ICQ_MSGxSMS:{
+            ICQSMS *sms = static_cast<ICQSMS*>(msg);
+            s += ParseText(sms->Message);
+            if (*sms->Phone.c_str()){
+                s += "<br>";
+                s += quoteText(sms->Phone);
+                if (*sms->Network.c_str())
+                    s += " (" + quoteText(sms->Network) + ")";
+            }
+            break;
+        }
+    case ICQ_MSGxWEBxPANEL:{
+            ICQWebPanel *m = static_cast<ICQWebPanel*>(msg);
+            s += ParseText(m->Message);
+            break;
+        }
+    case ICQ_MSGxEMAILxPAGER:{
+            ICQEmailPager *m = static_cast<ICQEmailPager*>(msg);
+            s += ParseText(m->Message);
+            break;
+        }
+    default:
+        log(L_WARN, "Unknown message type %u", msg->Type());
+        s += "???";
+    }
+    return s;
+}
+
+void TextShow::addMessageText(ICQMessage *msg)
+{
+    setText(text() + makeMessageText(msg));
+}
+
+QString TextShow::quoteText(const char *text)
+{
+    string msg;
+    pClient->quoteText(text, msg);
+    QString s = QString::fromLocal8Bit(msg.c_str());
+    return s;
+}
+
+MsgView::MsgView(QWidget *p)
+        : TextShow(p)
+{
+    bBack = false;
+    connect(pClient, SIGNAL(messageRead(ICQMessage*)), this, SLOT(messageRead(ICQMessage*)));
+    connect(pMain, SIGNAL(colorsChanged()), this, SLOT(colorsChanged()));
+    oldSendColor = pMain->ColorSend();
+    oldReceiveColor = pMain->ColorReceive();
+}
+
+static char FONT_FORMAT[] = "<font color=\"#%06X\">";
+
+void MsgView::colorsChanged()
+{
+    int x = contentsX();
+    int y = contentsY();
+    char FONT_SEND[] = "<##FontSend##>";
+    char FONT_RECEIVE[] = "<##FontReceive##>";
+    QString t = text();
+    QString c;
+    c.sprintf(FONT_FORMAT, oldSendColor);
+    t.replace(QRegExp(c), FONT_SEND);
+    c.sprintf(FONT_FORMAT, oldReceiveColor);
+    t.replace(QRegExp(c), FONT_RECEIVE);
+    c.sprintf(FONT_FORMAT, pMain->ColorSend());
+    t.replace(QRegExp(FONT_SEND), c);
+    c.sprintf(FONT_FORMAT, pMain->ColorReceive());
+    t.replace(QRegExp(FONT_RECEIVE), c);
+    setText(t);
+    setContentsPos(x, y);
+}
+
+void MsgView::messageRead(ICQMessage *msg)
+{
+    QString pat;
+    pat.sprintf("<p><a name=\"%lu.%lu\"></a>", msg->getUin(), msg->Id);
+    QString res;
+    QString t = text();
+    int pos = t.find(pat);
+    if (pos < 0) return;
+    res = t.left(pos);
+    if (msg){
+        bool bSaveBack = bBack;
+        bBack = false;
+        res += makeMessage(msg, false);
+        bBack = bSaveBack;
+    }
+    t = t.mid(pos+1);
+    pos = t.find("<p><a name=");
+    if (pos >= 0) res += t.mid(pos);
+    curAnchor = QString::number(msg->getUin()) + "." + QString::number(msg->Id);
+    setText(res, curAnchor);
+    scrollToAnchor(curAnchor);
+}
+
+void MsgView::deleteUser(unsigned long uin)
+{
+    QString pat;
+    pat.sprintf("<p><a name=\"%lu.", uin);
+    QString res;
+    QString t = text();
+    for (;;){
+        int pos = t.find(pat);
+        if (pos < 0){
+            res += t;
+            break;
+        }
+        res += t.left(pos);
+        t = t.mid(pos+1);
+        pos = t.find("<p><a name=");
+        if (pos >= 0){
+            t = t.mid(pos);
+        }else{
+            t = "";
+        }
+    }
+    setText(res, curAnchor);
+    scrollToAnchor(curAnchor);
+}
+
+void MsgView::setMessage(unsigned long uin, unsigned long msgId)
+{
+    QString pat;
+    pat.sprintf("<p><a name=\"%lu.%lu\"></a>", uin, msgId);
+    if (text().find(pat) < 0) return;
+    curAnchor = QString::number(uin) + "." + QString::number(msgId);
+    scrollToAnchor(curAnchor);
+}
+
+QString MsgView::makeMessage(ICQMessage *msg, bool bUnread)
+{
+    QString s;
+    s.sprintf("<p><a name=\"%lu.%lu\"></a>"
+              "<table width=100%%><tr>"
+              "<td><a href=\"msg://%lu.%lu\"><img src=\"icon:%s\"></a>&nbsp;",
+              msg->getUin(), msg->Id, msg->getUin(), msg->Id, Client::getMessageIcon(msg->Type()));
+    if (bUnread) s += "<b>";
+    QString color;
+    color.sprintf(FONT_FORMAT, msg->Received ? pMain->ColorReceive() : pMain->ColorSend());
+    s += color;
+    if (msg->Received){
+        CUser u(msg->getUin());
+        s += u.name(true);
+    }else{
+        CUser u(pClient);
+        s += u.name(true);
+    }
+    s += "</font>";
+    if (bUnread) s += "</b>";
+    s += "</td><td align=right>";
+    if (bUnread) s += "<b>";
+    QDateTime time;
+    time.setTime_t(msg->Time);
+#if USE_KDE
+    s += KGlobal::locale()->formatDateTime(time);
+#else
+    s += time.toString();
+#endif
+    if (bUnread) s += "</b>";
+    s += "</td></tr></table>";
+    unsigned long foreColor = 0;
+    unsigned long backColor = 0;
+    if (msg->Type() == ICQ_MSGxMSG){
+        ICQMsg *m = static_cast<ICQMsg*>(msg);
+        foreColor = m->ForeColor();
+        backColor = m->BackColor();
+    }
+    s += "<table width=100%";
+    if (foreColor != backColor){
+        QString bg;
+        bg.sprintf(" bgcolor=#%06lX", backColor);
+        s += bg;
+    }
+    s += "><tr><td>";
+    if (foreColor != backColor){
+        QString fg;
+        fg.sprintf("<font color=#%06lX>", foreColor);
+        s += fg;
+    }
+    s += makeMessageText(msg);
+    if (foreColor != backColor) s += "</font>";
+    s += "</td></tr></table></p>";
+    return s;
+}
+
+void MsgView::addMessage(ICQMessage *msg, bool bUnread, bool bSet)
+{
+    if (msg->Id >= MSG_PROCESS_ID) return;
+    int x = contentsX();
+    int y = contentsY();
+    QString s(makeMessage(msg, bUnread));
+    QString anchor;
+    if (bSet) QString::number(msg->getUin()) + "." + QString::number(msg->Id);
+    if (bBack){
+        setText(s + text(), anchor);
+    }else{
+        setText(text() + s, anchor);
+    }
+    if (bSet){
+        curAnchor = anchor;
+        scrollToAnchor(curAnchor);
+        return;
+    }
+    setContentsPos(x, y);
+}
+
+void MsgView::addUnread(unsigned long uin)
+{
+    ICQUser *u = pClient->getUser(uin);
+    if (u == NULL) return;
+    History h(uin);
+    for (list<unsigned long>::iterator it = u->unreadMsgs.begin(); it != u->unreadMsgs.end(); it++){
+        ICQMessage *msg = h.getMessage(*it);
+        if (msg == NULL) continue;
+        addMessage(msg, true, false);
+    }
+}
+
+HistoryView::HistoryView(QWidget *p, unsigned long uin)
+        : MsgView(p), m_nUin(uin)
+{
+    bBack = true;
+    ICQUser *u = pClient->getUser(uin);
+    if (u == NULL) return;
+    History h(uin);
+    History::iterator &it = h.messages();
+    list<unsigned long>::iterator unreadIt;
+    QString t;
+    for (++it; *it; ++it){
+        for (unreadIt = u->unreadMsgs.begin(); unreadIt != u->unreadMsgs.end(); unreadIt++)
+            if ((*unreadIt) == (*it)->Id) break;
+        t = makeMessage(*it, unreadIt != u->unreadMsgs.end()) + t;
+    }
+    setText(t);
+    connect(pClient, SIGNAL(messageReceived(ICQMessage*)), this, SLOT(messageReceived(ICQMessage*)));
+    connect(pClient, SIGNAL(event(ICQEvent*)), this, SLOT(processEvent(ICQEvent*)));
+}
+
+void HistoryView::processEvent(ICQEvent *e)
+{
+    if (e->type() != EVENT_DONE) return;
+    ICQMessage *msg = e->message();
+    if (msg == NULL) return;
+    messageReceived(msg);
+}
+
+void HistoryView::messageReceived(ICQMessage *msg)
+{
+    if (msg->getUin() != m_nUin) return;
+    if (msg->Id >= MSG_PROCESS_ID) return;
+    int x = contentsX();
+    int y = contentsY();
+    bool bUnread = false;
+    ICQUser *u = pClient->getUser(m_nUin);
+    if (u){
+        for (list<unsigned long>::iterator it = u->unreadMsgs.begin(); it != u->unreadMsgs.end(); it++){
+            if ((*it) == msg->Id){
+                bUnread = true;
+                break;
+            }
+        }
+    }
+    if (bBack){
+        QString saveText = text();
+        setText("");
+        addMessage(msg, bUnread, false);
+        y += contentsHeight();
+        setText(text() + saveText);
+    }else{
+        addMessage(msg, bUnread, false);
+    }
+    setContentsPos(x, y);
+}
+
+#ifndef _WINDOWS
+#include "msgview.moc"
+#endif
