8757e1d08163105d2ddb244a755e768056e2f61a
crz <crz@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2002-11-27 01:21:40 +0000

    kaddressbook integration. first approach
    git-svn-id: file:///.data/svn_convert/svn@579 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim/mainwin.cpp
+++ trunk/sim/mainwin.cpp
@@ -2571,11 +2571,160 @@ void MainWindow::monitorFinished()
     mNetMonitor = NULL;
 }
 
+#ifdef USE_KDE
+using namespace KABC;
+
+QString& getCountry(unsigned short code)
+{
+    int nC=0;
+    static QString str;
+    str.truncate(0);
+    
+    while (languages[nC].nCode)
+    {
+     if (languages[nC].nCode==code)
+        return str=languages[nC].szName;
+     nC++;
+    }
+
+    return str;
+}
+
+Addressee addresseeFromUser(SIMUser& u, Addressee* oldPers=NULL)
+{
+    Addressee pers;
+    QString str;
+
+    if (oldPers!=NULL)
+        pers=*oldPers;
+
+    if (pers.formattedName().isEmpty())
+        (u.FirstName.empty()&&u.LastName.empty())?pers.setFormattedName(QString::fromLocal8Bit(u.Nick.c_str())):pers.setNameFromString(QString::fromLocal8Bit((u.FirstName+" "+u.LastName).c_str()));
+
+    if (pers.nickName().isEmpty())
+        pers.setNickName(QString::fromLocal8Bit(u.Nick.c_str()));
+
+    pers.insertCustom("KADDRESSBOOK-X-CUSTOM","UIN",str.setNum(u.Uin));
+
+    if (pers.note().isEmpty())
+        pers.setNote(u.Notes);
+    
+    list<EMailInfo*>::iterator it=u.EMails.begin();
+
+    if (it!=u.EMails.end())
+    {
+        pers.insertEmail((*it)->Email.c_str(),true);
+        it++;
+        
+        while (it!=u.EMails.end())
+        {
+            pers.insertEmail((*it)->Email.c_str());
+            it++;
+        }
+    }
+
+    if (pers.url().isEmpty())
+        pers.setUrl(KURL(QString::fromLocal8Bit(u.Homepage.c_str())));
+
+    if (pers.organization().isEmpty())
+        pers.setOrganization(QString::fromLocal8Bit(u.WorkName.c_str()));
+
+    pers.insertPhoneNumber(PhoneNumber(u.HomePhone,PhoneNumber::Home));
+    pers.insertPhoneNumber(PhoneNumber(u.HomeFax,PhoneNumber::Home|PhoneNumber::Fax));
+    pers.insertPhoneNumber(PhoneNumber(u.PrivateCellular,PhoneNumber::Cell));
+    pers.insertPhoneNumber(PhoneNumber(u.WorkPhone,PhoneNumber::Work));
+    pers.insertPhoneNumber(PhoneNumber(u.WorkFax,PhoneNumber::Work|PhoneNumber::Fax));
+
+    Address home,work;
+    home.setType(Address::Home);
+    work.setType(Address::Work);
+
+    home.setLocality(QString::fromLocal8Bit(u.City.c_str()));
+    home.setRegion(QString::fromLocal8Bit(u.State.c_str()));
+    home.setStreet(QString::fromLocal8Bit(u.Address.c_str()));
+    home.setCountry(QString::fromLocal8Bit(getCountry(u.Country)));
+
+    work.setLocality(QString::fromLocal8Bit(u.WorkCity.c_str()));
+    work.setRegion(QString::fromLocal8Bit(u.WorkState.c_str()));
+    work.setStreet(QString::fromLocal8Bit(u.WorkAddress.c_str()));
+    work.setCountry(QString::fromLocal8Bit(getCountry(u.WorkCountry)));    
+
+    if (oldPers==NULL) // because i'm too laaaaaazy to handle this properly right now
+    {
+        pers.insertAddress(home);
+        pers.insertAddress(work);
+    }    
+
+    // TODO: handle all other fields (maybe)
+    
+    return pers;
+}
+#endif
+
 void MainWindow::doSynchronize()
 {
 #ifdef USE_KDE
-    //pClient->contacts->users
-    //ICQUser *u = pClient->getUser(uin)
+    if (!pClient->contacts.users.empty())
+    {
+        AddressBook& ab=*StdAddressBook::self();
+        bool bFound=false;
+        
+        list<ICQUser*>::iterator it=pClient->contacts.users.begin();
+        while (it!=pClient->contacts.users.end())
+        {
+            SIMUser& user=*(static_cast<SIMUser*>(*it));
+
+            Addressee newPers;
+            
+            if (!user.strKabUid.empty())
+            {
+                qDebug("not empty %s",user.strKabUid.c_str());
+                Addressee pers=ab.findByUid(user.strKabUid);
+
+                if (!pers.isEmpty())
+                {
+                    qDebug("but not found in addressbook");
+                    bFound=true;
+                    newPers=pers;
+                }
+            }
+            else
+            {
+                qDebug("empty, adding %lu",user.Uin);
+
+                list<EMailInfo*>::iterator it=user.EMails.begin();
+
+                while (it!=user.EMails.end())
+                {
+                    Addressee::List li=ab.findByEmail((*it)->Email.c_str());
+                    Addressee::List::iterator lit=li.begin();
+
+                    if (lit!=li.end())
+                    {
+                        qDebug("email found, actualy updating");
+                        bFound=true;
+                        newPers=(*lit);
+                        break;
+                    }
+                    
+                    it++;
+                }
+            }
+
+            Addressee pers;
+            
+            if (bFound)
+                pers=addresseeFromUser(user,&newPers);
+            else
+                pers=addresseeFromUser(user);
+
+            ab.insertAddressee(pers);
+            
+            it++;
+        }
+
+        StdAddressBook::save();
+    }
 #endif
 }
 
