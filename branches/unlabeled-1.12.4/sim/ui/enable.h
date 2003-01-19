33f62bcd5a415dd1ac487097955d9f0540b0e0ec
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-01-19 01:16:41 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.12.4'.
    git-svn-id: file:///.data/svn_convert/svn@878 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.12.4/sim/ui/enable.h
@@ -0,0 +1,46 @@
+#ifndef _ENABLE_H
+
+#include "defs.h"
+#include "country.h"
+
+#include <qwidget.h>
+
+#include <string>
+using namespace std;
+
+class QWidget;
+class QComboBox;
+
+void disableWidget(QWidget *);
+
+void initCombo(QComboBox *cmb, unsigned short code, const ext_info *tbl, bool bAddEmpty = true);
+void initTZCombo(QComboBox *cmb, char tz);
+
+unsigned short getComboValue(QComboBox *cmb, const ext_info *tbl);
+char getTZComboValue(QComboBox *cmb);
+
+void set(char **s, const QString &str);
+void set(string &s, const QString &str);
+void set(QString &s, const string &str);
+
+void setButtonsPict(QWidget *w);
+void raiseWindow(QWidget *w);
+
+#ifdef WIN32
+
+void setWndProc(QWidget*);
+void mySetCaption(QWidget *w, const QString &caption);
+
+#define SET_WNDPROC(A)	setWndProc(this);
+#define setCaption(s)	mySetCaption(this, s);
+
+#else
+
+void setWndClass(QWidget*, const char*);
+
+#define SET_WNDPROC(A)  setWndClass(this, A);
+
+#endif
+
+
+#endif
