9fbec165eeb5a6c13058dc53c6a41e7600f553f7
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-01-21 09:39:41 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.4'.
    git-svn-id: file:///.data/svn_convert/svn@895 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.4/sim/ui/moreinfo.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+                          moreinfo.h  -  description
+                             -------------------
+    begin                : Sun Mar 24 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _MOREINFO_H
+#define _MOREINFO_H
+
+#include "defs.h"
+#include "moreinfobase.h"
+
+class ICQUser;
+class ICQGroup;
+class QComboBox;
+
+class MoreInfo : public MoreInfoBase
+{
+    Q_OBJECT
+public:
+    MoreInfo(QWidget *p, bool readOnly=false);
+public slots:
+    void apply(ICQUser *u);
+    void load(ICQUser *u);
+    void save(ICQUser *u);
+    void load(ICQGroup *g);
+    void save(ICQGroup *g);
+protected slots:
+    void birthDayChanged(int);
+    void setLang(int);
+    void goUrl();
+};
+
+#endif
+
