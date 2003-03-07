80e5ca7f81f64d22b4bfdb7e9d329426ce9f10c7
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-03-07 02:35:44 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.10.4'.
    git-svn-id: file:///.data/svn_convert/svn@923 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.10.4/sim/control.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+                          control.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _CONTROL_H
+#define _CONTROL_H
+
+#include "defs.h"
+#include <qsocketdevice.h>
+#include <string>
+#include <list>
+using namespace std;
+
+class QSocketNotifier;
+class ControlSocket;
+class ICQEvent;
+
+class ControlListener : public QObject
+{
+    Q_OBJECT
+public:
+    ControlListener(QObject *parent);
+    ~ControlListener();
+    bool bind(const char *addr);
+protected slots:
+    void activated(int);
+    void finished(ControlSocket*);
+    void finished();
+protected:
+    string local_name;
+    list<ControlSocket*> deleted;
+    bool setOptions(int nPort);
+    QSocketNotifier *n;
+    int s;
+};
+
+class ControlSocket : public QObject
+{
+    Q_OBJECT
+public:
+    ControlSocket(int s, QObject *parent);
+    ~ControlSocket();
+signals:
+    void finished(ControlSocket*);
+protected slots:
+    void read_ready(int);
+    void processEvent(ICQEvent*);
+    void reset();
+    void iconChanged();
+protected:
+    int  s;
+    bool bEscape;
+    bool bNotify;
+    string read_line;
+    QString icons;
+    QString status;
+    QString msg;
+    void write(const char*);
+    ICQEvent *sendEvent;
+};
+
+#endif
+
