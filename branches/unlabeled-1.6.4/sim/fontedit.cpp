aadf5c6556d21f620d89103f3e06d6453b7d521e
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-04-14 17:09:06 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.6.4'.
    git-svn-id: file:///.data/svn_convert/svn@949 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.6.4/sim/fontedit.cpp
@@ -0,0 +1,70 @@
+/***************************************************************************
+                          fontedit.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "fontedit.h"
+#include "mainwin.h"
+#include "icons.h"
+
+#include <qlayout.h>
+#include <qpushbutton.h>
+#include <qlabel.h>
+
+#ifdef USE_KDE
+#include <kfontdialog.h>
+#else
+#include <qfontdialog.h>
+#endif
+
+FontEdit::FontEdit(QWidget *parent, const char *name) : QFrame(parent, name)
+{
+    QHBoxLayout *lay = new QHBoxLayout(this);
+    lblFont = new QLabel("...", this);
+    lblFont->setSizePolicy(QSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum));
+    lay->addWidget(lblFont);
+    lay->addSpacing(2);
+    QPushButton *btnFont = new QPushButton(this);
+    btnFont->setPixmap(Pict("text"));
+    lay->addWidget(btnFont);
+    lblFont->setFrameShape(QFrame::Box);
+    lblFont->setLineWidth(1);
+    lblFont->setMargin(3);
+    connect(btnFont, SIGNAL(clicked()), this, SLOT(chooseFont()));
+}
+
+void FontEdit::setWinFont(const QFont &_f)
+{
+    f = _f;
+    QString fontName = pMain->font2str(f, true);
+    lblFont->setText(fontName);
+}
+
+void FontEdit::chooseFont()
+{
+#ifdef USE_KDE
+    QFont _f = f;
+    if (KFontDialog::getFont(_f, false, topLevelWidget()) == KFontDialog::Accepted)
+        setWinFont(_f);
+#else
+    bool ok = false;
+    QFont _f = QFontDialog::getFont(&ok, f, topLevelWidget());
+    if (ok) setWinFont(_f);
+#endif
+}
+
+#ifndef _WINDOWS
+#include "fontedit.moc"
+#endif
