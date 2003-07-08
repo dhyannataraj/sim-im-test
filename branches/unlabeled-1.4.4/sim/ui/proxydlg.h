98294b1037f6c03cd1254c171d30b91dacb57200
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-07-08 07:27:01 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.4'.
    git-svn-id: file:///.data/svn_convert/svn@984 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.4.4/sim/ui/proxydlg.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+                          proxydlg.h  -  description
+                             -------------------
+    begin                : Sun Mar 24 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _PROXYDLG_H
+#define _PROXYDLG_H
+
+#include "defs.h"
+#include "proxydlgbase.h"
+
+class ProxyDialog : public ProxyDlgBase
+{
+    Q_OBJECT
+public:
+    ProxyDialog(QWidget *p, const QString &msg = QString::null);
+protected slots:
+    void apply();
+    void proxyChanged(bool);
+    void proxyChanged(int);
+};
+
+#endif
+
---
+++ branches/unlabeled-1.4.4/sim/ui/workinfo.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+                          workinfo.h  -  description
+                             -------------------
+    begin                : Sun Mar 24 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _WORKINFO_H
+#define _WORKINFO_H
+
+#include "defs.h"
+#include "workinfobase.h"
+
+class ICQUser;
+class ICQGroup;
+
+class WorkInfo : public WorkInfoBase
+{
+    Q_OBJECT
+public:
+    WorkInfo(QWidget *p, bool readOnly=false);
+public slots:
+    void apply(ICQUser*);
+    void load(ICQUser*);
+    void save(ICQUser*);
+    void load(ICQGroup*);
+    void save(ICQGroup*);
+protected slots:
+    void goUrl();
+};
+
+#endif
+
