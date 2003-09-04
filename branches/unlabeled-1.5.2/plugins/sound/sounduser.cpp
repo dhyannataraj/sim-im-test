1d8ac1f952b264cf7b9440540415a5d311832753
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-09-04 05:38:04 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.
    git-svn-id: file:///.data/svn_convert/svn@2155 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.2/plugins/sound/sounduser.cpp
@@ -0,0 +1,95 @@
+/***************************************************************************
+                          sounduser.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "sounduser.h"
+#include "sound.h"
+#include "editfile.h"
+#include "core.h"
+
+#include <qcheckbox.h>
+#include <qlabel.h>
+#include <qlineedit.h>
+#include <qlayout.h>
+
+SoundUserConfig::SoundUserConfig(QWidget *parent, void *data, SoundPlugin *plugin)
+        : SoundUserConfigBase(parent)
+{
+    m_plugin = plugin;
+    SoundUserData *user_data = (SoundUserData*)data;
+    string s;
+    s = plugin->fullName(user_data->Alert);
+    edtAlert->setText(QFile::decodeName(s.c_str()));
+    QGridLayout *lay = static_cast<QGridLayout*>(layout());
+    if (lay == NULL)
+        return;
+    unsigned n = 1;
+    CommandDef *cmd;
+    CommandsMapIterator it(m_plugin->core->messageTypes);
+    while ((cmd = ++it) != NULL){
+        MessageDef *def = (MessageDef*)(cmd->param);
+        if ((def == NULL) || (def->base_type) || (cmd->icon == NULL) || (def->flags & MESSAGE_HIDDEN))
+            continue;
+        QLabel *lbl = new QLabel(this);
+        lbl->setAlignment(AlignRight);
+        lbl->setText(i18n(def->singular, def->plural, 1) + ":");
+        lay->addWidget(lbl, n, 0);
+        EditSound *snd = new EditSound(this);
+        snd->setText(QFile::decodeName(m_plugin->messageSound(cmd->id, user_data).c_str()));
+        lay->addWidget(snd, n, 1);
+        m_sounds.insert(MAP_SOUND::value_type(cmd->id, snd));
+        n++;
+    }
+    lay->addItem(new QSpacerItem(n, 1, QSizePolicy::Minimum, QSizePolicy::Expanding));
+    chkActive->setChecked(user_data->NoSoundIfActive);
+    chkDisable->setChecked(user_data->Disable);
+    connect(chkDisable, SIGNAL(toggled(bool)), this, SLOT(toggled(bool)));
+    toggled(user_data->Disable);
+}
+
+void SoundUserConfig::apply(void *data)
+{
+    SoundUserData *user_data = (SoundUserData*)data;
+    set_str(&user_data->Alert, QFile::encodeName(edtAlert->text()));
+    CommandDef *cmd;
+    CommandsMapIterator it(m_plugin->core->messageTypes);
+    while ((cmd = ++it) != NULL){
+        MessageDef *def = (MessageDef*)(cmd->param);
+        if (def->base_type)
+            continue;
+        MAP_SOUND::iterator it = m_sounds.find(cmd->id);
+        if (it == m_sounds.end())
+            continue;
+        EditSound *snd = (*it).second;
+        QString text = snd->text();
+        if (text.isEmpty())
+            text = "-";
+        set_str(&user_data->Receive, cmd->id, QFile::encodeName(text));
+    }
+    user_data->NoSoundIfActive = chkActive->isChecked();
+    user_data->Disable = chkDisable->isChecked();
+}
+
+void SoundUserConfig::toggled(bool bState)
+{
+    for (MAP_SOUND::iterator it = m_sounds.begin(); it != m_sounds.end(); ++it)
+        (*it).second->setEnabled(!bState);
+}
+
+#ifndef WIN32
+#include "sounduser.moc"
+#endif
+
