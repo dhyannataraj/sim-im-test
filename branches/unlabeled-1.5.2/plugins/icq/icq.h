a2c31732213ac4b213bcd25445d46d97c2ded585
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-09-14 20:08:38 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.
    git-svn-id: file:///.data/svn_convert/svn@2290 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.2/plugins/icq/icq.h
@@ -0,0 +1,88 @@
+/***************************************************************************
+                          icq.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _ICQ_H
+#define _ICQ_H
+
+#include "simapi.h"
+
+class ICQProtocol : public Protocol
+{
+public:
+    ICQProtocol(Plugin *plugin);
+    ~ICQProtocol();
+    Client	*createClient(const char *cfg);
+    const CommandDef *description();
+    const CommandDef *statusList();
+    virtual const DataDef *userDataDef();
+    static const DataDef *icqUserData;
+};
+
+class AIMProtocol : public Protocol
+{
+public:
+    AIMProtocol(Plugin *plugin);
+    ~AIMProtocol();
+    Client	*createClient(const char *cfg);
+    const CommandDef *description();
+    const CommandDef *statusList();
+    virtual const DataDef *userDataDef();
+    static const DataDef *icqUserData;
+};
+
+typedef struct ICQData
+{
+    unsigned	ShowAllEncodings;
+} ICQData;
+
+class ICQPlugin : public Plugin
+{
+public:
+    ICQPlugin(unsigned base, const char *cfg);
+    virtual ~ICQPlugin();
+    unsigned ICQPacket;
+    unsigned ICQDirectPacket;
+    unsigned AIMPacket;
+    PROP_BOOL(ShowAllEncodings);
+    unsigned EventSearch;
+    unsigned EventSearchDone;
+    unsigned EventAutoReplyFail;
+    unsigned EventRandomChat;
+    unsigned EventRandomChatInfo;
+    unsigned CmdVisibleList;
+    unsigned CmdInvisibleList;
+    unsigned CmdChangeEncoding;
+    unsigned CmdAllEncodings;
+    unsigned CmdSendMessage;
+    unsigned CmdGroups;
+    unsigned CmdCheckInvisible;
+    unsigned MenuEncoding;
+    unsigned MenuSearchResult;
+    unsigned MenuGroups;
+    unsigned RetrySendDND;
+    unsigned RetrySendOccupied;
+    static Protocol *m_icq;
+    static Protocol *m_aim;
+    void registerMessages();
+    void unregisterMessages();
+protected:
+    virtual string getConfig();
+    ICQData	 data;
+};
+
+#endif
+
