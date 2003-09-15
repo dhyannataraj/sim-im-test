fac94f22e690fe28a400436ce59f1219d7200101
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-09-15 06:58:46 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.2'.
    git-svn-id: file:///.data/svn_convert/svn@2303 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.4.2/plugins/jabber/jabberconfig.cpp
@@ -0,0 +1,143 @@
+/***************************************************************************
+                          jabberconfig.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "jabberconfig.h"
+#include "jabberclient.h"
+#include "jabber.h"
+#include "linklabel.h"
+
+#include <qtimer.h>
+#include <qlineedit.h>
+#include <qcheckbox.h>
+#include <qlabel.h>
+#include <qspinbox.h>
+#include <qtabwidget.h>
+
+JabberConfig::JabberConfig(QWidget *parent, JabberClient *client, bool bConfig)
+        : JabberConfigBase(parent)
+{
+    m_client = client;
+    m_bConfig = bConfig;
+    QTimer::singleShot(0, this, SLOT(changed()));
+    edtID->setText(m_client->getID());
+    edtPasswd->setText(m_client->getPassword());
+    edtServer->setText(QString::fromLocal8Bit(m_client->getServer()));
+    edtPort->setValue(m_client->getPort());
+    if (m_client->data.owner.Resource)
+        edtResource->setText(QString::fromUtf8(m_client->data.owner.Resource));
+    if (m_client->data.VHost)
+        edtVHost->setText(QString::fromUtf8(m_client->data.VHost));
+    if (m_bConfig)
+        tabCfg->removePage(tabJabber);
+#ifdef USE_OPENSSL
+    chkSSL->setChecked(m_client->getUseSSL());
+    chkPlain->setChecked(m_client->getUsePlain());
+#else
+    chkSSL->hide();
+    chkPlain->hide();
+#endif
+    chkVHost->setChecked(m_client->getUseVHost());
+    lnkPublic->setText(i18n("List of public servers"));
+    connect(lnkPublic, SIGNAL(click()), this, SLOT(showPublic()));
+    connect(edtID, SIGNAL(textChanged(const QString&)), this, SLOT(changed(const QString&)));
+    connect(edtPasswd, SIGNAL(textChanged(const QString&)), this, SLOT(changed(const QString&)));
+    connect(edtServer, SIGNAL(textChanged(const QString&)), this, SLOT(changed(const QString&)));
+    connect(edtPort, SIGNAL(valueChanged(const QString&)), this, SLOT(changed(const QString&)));
+    connect(chkSSL, SIGNAL(toggled(bool)), this, SLOT(toggledSSL(bool)));
+    connect(chkVHost, SIGNAL(toggled(bool)), this, SLOT(toggledVHost(bool)));
+}
+
+void JabberConfig::apply(Client*, void*)
+{
+}
+
+void JabberConfig::apply()
+{
+    QString jid = edtID->text();
+    if (jid.find('@') < 0){
+        QString host;
+        if (chkVHost->isChecked() && !edtVHost->text().isEmpty()){
+            host = edtVHost->text();
+        }else{
+            host = edtServer->text();
+        }
+        if (!host.isEmpty()){
+            jid += "@";
+            jid += host;
+        }
+    }
+    if (!m_bConfig){
+        m_client->setID(jid);
+        m_client->setPassword(edtPasswd->text());
+        m_client->setRegister(chkRegister->isChecked());
+    }
+    m_client->setServer(edtServer->text().local8Bit());
+    m_client->setPort(atol(edtPort->text()));
+#ifdef USE_OPENSSL
+    m_client->setUseSSL(chkSSL->isChecked());
+    m_client->setUsePlain(chkPlain->isChecked());
+#endif
+    m_client->setUseVHost(chkVHost->isChecked());
+    set_str(&m_client->data.owner.Resource, edtResource->text().utf8());
+    if (chkVHost->isChecked())
+        set_str(&m_client->data.VHost, edtVHost->text().utf8());
+}
+
+void JabberConfig::toggledSSL(bool bState)
+{
+    unsigned port = atol(edtPort->text());
+    if (port == 0)
+        port = 5222;
+    if (bState){
+        port++;
+    }else{
+        port--;
+    }
+    edtPort->setValue(port);
+}
+
+void JabberConfig::toggledVHost(bool bState)
+{
+    edtVHost->setEnabled(bState);
+}
+
+void JabberConfig::changed(const QString&)
+{
+    changed();
+}
+
+void JabberConfig::changed()
+{
+    bool bOK =  !edtID->text().isEmpty() &&
+                !edtPasswd->text().isEmpty() &&
+                !edtServer->text().isEmpty() &&
+                atol(edtPort->text());
+    emit okEnabled(bOK);
+}
+
+void JabberConfig::showPublic()
+{
+    Event e(EventGoURL, "http://www.jabber.org/user/publicservers.php");
+    e.process();
+}
+
+
+#ifndef WIN32
+#include "jabberconfig.moc"
+#endif
+
+
---
+++ branches/unlabeled-1.4.2/plugins/msn/msn.cpp
@@ -0,0 +1,298 @@
+/***************************************************************************
+                          msn.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "msn.h"
+#include "msnclient.h"
+#include "simapi.h"
+
+#include "xpm/msn_online.xpm"
+#include "xpm/msn_offline.xpm"
+#include "xpm/msn_away.xpm"
+#include "xpm/msn_na.xpm"
+#include "xpm/msn_dnd.xpm"
+#include "xpm/msn_invisible.xpm"
+
+Plugin *createMSNPlugin(unsigned base, bool, const char*)
+{
+    Plugin *plugin = new MSNPlugin(base);
+    return plugin;
+}
+
+static PluginInfo info =
+    {
+        NULL,
+        NULL,
+        VERSION,
+        createMSNPlugin,
+        PLUGIN_PROTOCOL
+    };
+
+EXPORT_PROC PluginInfo* GetPluginInfo()
+{
+    return &info;
+}
+
+MSNProtocol::MSNProtocol(Plugin *plugin)
+        : Protocol(plugin)
+{
+}
+
+MSNProtocol::~MSNProtocol()
+{
+}
+
+Client *MSNProtocol::createClient(const char *cfg)
+{
+    return new MSNClient(this, cfg);
+}
+
+static CommandDef msn_descr =
+    {
+        0,
+        I18N_NOOP("MSN"),
+        "MSN_online",
+        "MSN_invisible",
+        I18N_NOOP("http://help.microsoft.com/!data/en_us/data/messengerv47_xp.its51/$content$/xptroublesignin.htm"),
+        0,
+        0,
+        0,
+        0,
+        0,
+        PROTOCOL_SEARCH_ONLINE | PROTOCOL_INVISIBLE,
+        NULL,
+        NULL
+    };
+
+const CommandDef *MSNProtocol::description()
+{
+    return &msn_descr;
+}
+
+static CommandDef msn_status_list[] =
+    {
+        {
+            STATUS_ONLINE,
+            I18N_NOOP("Online"),
+            "MSN_online",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            STATUS_AWAY,
+            I18N_NOOP("Away"),
+            "MSN_away",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            STATUS_NA,
+            I18N_NOOP("N/A"),
+            "MSN_na",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            STATUS_DND,
+            I18N_NOOP("Busy"),
+            "MSN_dnd",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            STATUS_BRB,
+            I18N_NOOP("Be right back"),
+            "MSN_away",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            STATUS_PHONE,
+            I18N_NOOP("On the phone"),
+            "MSN_away",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            STATUS_LUNCH,
+            I18N_NOOP("On the lunch"),
+            "MSN_away",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            STATUS_OFFLINE,
+            I18N_NOOP("Offline"),
+            "MSN_offline",
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        },
+        {
+            0,
+            NULL,
+            NULL,
+            NULL,
+            NULL,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            NULL,
+            NULL
+        }
+    };
+
+const CommandDef *MSNProtocol::statusList()
+{
+    return msn_status_list;
+}
+
+MSNPlugin::MSNPlugin(unsigned base)
+        : Plugin(base)
+{
+    MSNPacket = registerType();
+    getContacts()->addPacketType(MSNPacket, msn_descr.text, PACKET_TEXT);
+
+    IconDef icon;
+    icon.name = "MSN_online";
+    icon.xpm = msn_online;
+    icon.isSystem = false;
+
+    Event eIcon(EventAddIcon, &icon);
+    eIcon.process();
+
+    icon.name = "MSN_offline";
+    icon.xpm = msn_offline;
+    icon.isSystem = false;
+    eIcon.process();
+
+    icon.name = "MSN_away";
+    icon.xpm = msn_away;
+    icon.isSystem = false;
+    eIcon.process();
+
+    icon.name = "MSN_na";
+    icon.xpm = msn_na;
+    icon.isSystem = false;
+    eIcon.process();
+
+    icon.name = "MSN_dnd";
+    icon.xpm = msn_dnd;
+    icon.isSystem = false;
+    eIcon.process();
+
+    icon.name = "MSN_invisible";
+    icon.xpm = msn_invisible;
+    icon.isSystem = false;
+    eIcon.process();
+
+    m_protocol = new MSNProtocol(this);
+}
+
+MSNPlugin::~MSNPlugin()
+{
+    getContacts()->removePacketType(MSNPacket);
+    delete m_protocol;
+}
+
+#ifdef WIN32
+#include <windows.h>
+
+/**
+ * DLL's entry point
+ **/
+int WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+    return TRUE;
+}
+
+/**
+ * This is to prevent the CRT from loading, thus making this a smaller
+ * and faster dll.
+ **/
+extern "C" BOOL __stdcall _DllMainCRTStartup( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    return DllMain( hinstDLL, fdwReason, lpvReserved );
+}
+
+#endif
+
+
