1b5afbf29619e67b4605e3fc92ec85f00a10cc48
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-09-29 07:59:21 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.3.2'.
    git-svn-id: file:///.data/svn_convert/svn@2434 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.3.2/plugins/dock/dockwnd.h
@@ -0,0 +1,81 @@
+/***************************************************************************
+                          dockwnd.h  -  description
+                             -------------------
+    begin                : Sun Mar 10 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _DOCKWND_H
+#define _DOCKWND_H
+
+#include "simapi.h"
+
+#include <qwidget.h>
+#include <qtooltip.h>
+#include <qpixmap.h>
+
+class WharfIcon;
+class DockPlugin;
+
+class DockWnd : public QWidget, public EventReceiver
+{
+    Q_OBJECT
+public:
+    DockWnd(DockPlugin *plugin, const char *icon, const char *text);
+    ~DockWnd();
+    void setIcon(const char *icon);
+    void setTip(const char *text);
+#ifdef WIN32
+    void callProc(unsigned long);
+#endif
+    void mouseEvent( QMouseEvent *e);
+    virtual void mouseDoubleClickEvent( QMouseEvent *e);
+signals:
+    void showPopup(QPoint);
+    void toggleWin();
+    void doubleClicked();
+protected slots:
+    void toggle();
+    void blink();
+protected:
+    virtual void *processEvent(Event *e);
+    void  reset();
+    bool  bNoToggle;
+    const char *m_tip;
+    const char *m_state;
+    const char *m_unread;
+    QString m_unreadText;
+    QPixmap drawIcon;
+    virtual void enterEvent( QEvent *e);
+    virtual void paintEvent( QPaintEvent *e);
+    virtual void mousePressEvent( QMouseEvent *e);
+    virtual void mouseReleaseEvent( QMouseEvent *e);
+    virtual void mouseMoveEvent( QMouseEvent *e);
+    void quit();
+    bool bBlink;
+    QTimer *blinkTimer;
+#ifndef WIN32
+    QPoint mousePos;
+#ifndef QT_MACOSX_VERSION
+    WharfIcon *wharfIcon;
+    bool x11Event(XEvent*);
+    bool inTray;
+    bool inNetTray;
+    bool bInit;
+#endif
+#endif
+    DockPlugin *m_plugin;
+};
+
+#endif
+
---
+++ branches/unlabeled-1.3.2/sim/ui/textshow.h
@@ -0,0 +1,157 @@
+/***************************************************************************
+                          textshow.h  -  description
+                             -------------------
+    begin                : Sat Mar 16 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _TEXTSHOW_H
+#define _TEXTSHOW_H
+
+#include "simapi.h"
+
+#include <qglobal.h>
+#include <qmainwindow.h>
+#include <qtoolbutton.h>
+#include <qlabel.h>
+
+#if QT_VERSION < 300
+#include "qt3/qtextbrowser.h"
+#else
+#include <qtextbrowser.h>
+#ifdef USE_KDE
+#ifdef HAVE_KTEXTBROWSER_H
+#include <ktextbrowser.h>
+#define QTextBrowser KTextBrowser
+#endif
+#endif
+#endif
+
+class UI_EXPORT TextShow : public QTextBrowser
+{
+    Q_OBJECT
+public:
+    TextShow (QWidget *parent, const char *name=NULL);
+    ~TextShow();
+    QString quoteText(const char *text, const char *charset=NULL);
+    void copy();
+    QString selectedText();
+    QString plainText(int paraFrom=0, int paraTo=0x7FFFFFFF, int indexFrom=0, int indexTo=0);
+    void setText(const QString &text);
+    static unsigned textPosition(const QString &text, unsigned pos);
+    static QString unquoteString(const QString &s, int from, int to);
+signals:
+    void showPopup(QPoint);
+protected:
+    void startDrag();
+    void keyPressEvent(QKeyEvent *e);
+    void resizeEvent(QResizeEvent *e);
+    void setSource(const QString&);
+};
+
+class UI_EXPORT TextEdit : public TextShow
+{
+    Q_OBJECT
+public:
+    TextEdit(QWidget *parent, const char *name = NULL);
+    ~TextEdit();
+    void setCtrlMode(bool);
+    const QColor &background() const;
+    const QColor &foreground() const;
+    void setForeground(const QColor&);
+    void setBackground(const QColor&);
+    void setTextFormat(QTextEdit::TextFormat);
+    void changeText();
+signals:
+    void ctrlEnterPressed();
+    void lostFocus();
+protected:
+    bool eventFilter(QObject *o, QEvent *e);
+    void keyPressEvent(QKeyEvent *e);
+    QColor curFG;
+    bool m_bCtrlMode;
+};
+
+class QToolBar;
+
+class UI_EXPORT ColorLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    ColorLabel(QWidget *parent, QColor c, int id, const QString&);
+signals:
+    void selected(int);
+protected:
+    void mouseReleaseEvent(QMouseEvent*);
+    QSize sizeHint() const;
+    QSize minimumSizeHint() const;
+    unsigned m_id;
+};
+
+class UI_EXPORT ColorPopup : public QFrame
+{
+    Q_OBJECT
+public:
+    ColorPopup(QWidget *parent, QColor c);
+signals:
+    void colorChanged(QColor color);
+    void colorCustom();
+protected slots:
+    void colorSelected(int);
+};
+
+class UI_EXPORT ColorToolButton : public QToolButton
+{
+    Q_OBJECT
+public:
+    ColorToolButton(QWidget *parent, QColor color);
+signals:
+    void colorChanged(QColor color);
+protected slots:
+    void btnClicked();
+    void selectColor(QColor);
+    void selectCustom();
+    void closePopup();
+protected:
+    ColorPopup *m_popup;
+    QColor m_color;
+};
+
+class UI_EXPORT RichTextEdit : public QMainWindow
+{
+    Q_OBJECT
+public:
+    RichTextEdit(QWidget *parent, const char *name = NULL);
+    void setText(const QString&);
+    QString text();
+    void setTextFormat(QTextEdit::TextFormat);
+    QTextEdit::TextFormat textFormat();
+    void setReadOnly(bool bState);
+    void showBar();
+protected slots:
+    void toggleBold(bool);
+    void toggleItalic(bool);
+    void toggleUnderline(bool);
+    void bgColorChanged(QColor);
+    void fgColorChanged(QColor);
+    void fontChanged(const QFont&);
+    void selectFont();
+protected:
+    TextEdit	*m_edit;
+    QToolBar	*m_bar;
+    QToolButton	*btnBold;
+    QToolButton	*btnItalic;
+    QToolButton	*btnUnderline;
+};
+
+#endif
