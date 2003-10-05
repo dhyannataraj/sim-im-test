846d6068b889ff168059f1de185c0deab4dcb6be
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-10-05 18:56:15 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.8.2'.
    git-svn-id: file:///.data/svn_convert/svn@2505 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.8.2/sim/ui/ballonmsg.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+                          ballonmsg.h  -  description
+                             -------------------
+    begin                : Sun Mar 24 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _BALLONMSG_H
+#define _BALLONMSG_H
+
+#include "simapi.h"
+#include <qstring.h>
+#include <qdialog.h>
+#include <qbitmap.h>
+#include <qpushbutton.h>
+
+class QStringList;
+
+class UI_EXPORT BalloonMsg : public QDialog
+{
+    Q_OBJECT
+public:
+    BalloonMsg(void *param, const QString &text, QStringList&, QWidget *p, const QRect *rc = NULL, bool bModal=false, bool bAutoHide=true);
+    ~BalloonMsg();
+    static void message(const QString &text, QWidget *parent, bool bModal=false);
+    static void ask(void *param, const QString &text, QWidget *parent, const char *slotYes, const char *slotNo, const QRect *rc=NULL, QObject *receiver=NULL);
+signals:
+    void action(int, void*);
+    void yes_action(void*);
+    void no_action(void*);
+    void destroyed();
+protected slots:
+    void action(int);
+protected:
+    bool eventFilter(QObject*, QEvent*);
+    void paintEvent(QPaintEvent*);
+    void mousePressEvent(QMouseEvent*);
+    QString text;
+    QRect textRect;
+    QBitmap mask;
+    QWidget *m_parent;
+    bool m_bAutoHide;
+    bool m_bYes;
+    void *m_param;
+};
+
+class BalloonButton : public QPushButton
+{
+    Q_OBJECT
+public:
+    BalloonButton(QString, QWidget*, int);
+signals:
+    void action(int);
+protected slots:
+    void click();
+protected:
+    int id;
+};
+
+
+#endif
+
