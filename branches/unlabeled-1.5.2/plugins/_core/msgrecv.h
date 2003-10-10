ab0b127088efd897f52b94d646770144765a82d9
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-10-10 07:21:45 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.
    git-svn-id: file:///.data/svn_convert/svn@2557 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.2/plugins/_core/msgrecv.h
@@ -0,0 +1,68 @@
+/***************************************************************************
+                          msgrecv.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _MSGRECV_H
+#define _MSGRECV_H
+
+#include "simapi.h"
+
+#include <qtoolbutton.h>
+
+#include <list>
+using namespace std;
+
+class CToolCustom;
+class MsgEdit;
+class QBoxLayout;
+
+class CmdButton : public QToolButton
+{
+    Q_OBJECT
+public:
+    CmdButton(CToolCustom *parent, unsigned id, const char *text);
+    unsigned id() { return m_id; }
+signals:
+    void command(CmdButton*);
+protected slots:
+    void click();
+protected:
+    QSize sizeHint() const;
+    QSize minimumSizeHint() const;
+    void paintEvent(QPaintEvent*);
+    void setText();
+    const char *m_text;
+    unsigned   m_id;
+};
+
+class MsgReceived : public QObject
+{
+    Q_OBJECT
+public:
+    MsgReceived(CToolCustom *parent, Message *msg);
+protected slots:
+    void command(CmdButton*);
+    void textChanged();
+    void init();
+protected:
+    unsigned	m_id;
+    unsigned	m_contact;
+    string		m_client;
+    MsgEdit	*m_edit;
+};
+
+#endif
+
