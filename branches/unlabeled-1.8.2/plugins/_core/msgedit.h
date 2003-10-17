27a7fd025c3da5672f43b62486a174012a39ca60
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-10-17 05:22:26 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.8.2'.
    git-svn-id: file:///.data/svn_convert/svn@2621 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.8.2/plugins/_core/msgedit.h
@@ -0,0 +1,117 @@
+/***************************************************************************
+                          msgedit.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _MSGEDIT_H
+#define _MSGEDIT_H
+
+#include "simapi.h"
+#include "core.h"
+
+#include <qmainwindow.h>
+#include <qlabel.h>
+
+#include <list>
+using namespace std;
+
+class CorePlugin;
+class UserWnd;
+class CToolBar;
+class QVBoxLayout;
+class QFrame;
+class TextEdit;
+
+typedef struct ClientStatus
+{
+    unsigned long	status;
+    unsigned		client;
+    clientData		*data;
+} ClientStatus;
+
+class MsgEdit : public QMainWindow, public EventReceiver
+{
+    Q_OBJECT
+public:
+    MsgEdit(QWidget *parent, UserWnd *userWnd, bool bReceived);
+    ~MsgEdit();
+    CToolBar	*m_bar;
+    bool setMessage(Message *msg, bool bSetFocus);
+    UserWnd		*m_userWnd;
+    TextEdit	*m_edit;
+    QVBoxLayout	*m_layout;
+    QFrame		*m_frame;
+    bool sendMessage(Message *msg);
+    static void setupMessages();
+    void		getWays(vector<ClientStatus> &cs, Contact *contact);
+    Client		*client(void *&data, bool bCreate, bool bSendTyping, unsigned contact_id);
+    bool		m_bReceived;
+    unsigned	m_flags;
+signals:
+    void heightChanged(int);
+    void init();
+public slots:
+    void insertSmile(int id);
+    void modeChanged();
+    void editLostFocus();
+    void editTextChanged();
+    void editEnterPressed();
+    void setInput();
+    void goNext();
+    void setupNext();
+protected:
+    unsigned m_type;
+    void *processEvent(Event*);
+    void resizeEvent(QResizeEvent*);
+    void stopSend(bool bCheck=true);
+    void showCloseSend(bool bShow);
+    void typingStart();
+    void typingStop();
+    void changeTyping(Client *client, void *data);
+    bool	m_bTyping;
+    string	m_typingClient;
+    bool send();
+    list<unsigned> multiply;
+    list<unsigned>::iterator multiply_it;
+    Message		*m_msg;
+    MsgSend		m_retry;
+    string m_client;
+};
+
+class SmileLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    SmileLabel(int id, const char *tip, QWidget *parent);
+signals:
+    void clicked(int id);
+protected:
+    void mouseReleaseEvent(QMouseEvent*);
+    int id;
+};
+
+class SmilePopup : public QFrame
+{
+    Q_OBJECT
+public:
+    SmilePopup(QWidget *parent);
+signals:
+    void insert(int id);
+protected slots:
+    void labelClicked(int id);
+};
+
+#endif
+
