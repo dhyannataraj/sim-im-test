1e6b9d753263932697c2037f5e0801461eb473f9
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-10-27 16:43:38 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.6.2'.
    git-svn-id: file:///.data/svn_convert/svn@2743 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.6.2/plugins/jabber/jabbermessage.h
@@ -0,0 +1,86 @@
+/***************************************************************************
+                          jabbermessage.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _JABBERMESSAGE_H
+#define _JABBERMESSAGE_H
+
+#include "simapi.h"
+
+const unsigned MessageJabber		= 0x201;
+const unsigned MessageJabberOnline	= 0x202;
+const unsigned MessageJabberOffline	= 0x203;
+const unsigned MessageJabberError	= 0x204;
+
+typedef struct JabberMessageData
+{
+    char	*Subject;
+} JabberMessageData;
+
+class JabberMessage : public Message
+{
+public:
+    JabberMessage(const char *cfg = NULL);
+    ~JabberMessage();
+    PROP_UTF8(Subject);
+    virtual string save();
+    virtual QString presentation();
+protected:
+    JabberMessageData	data;
+};
+
+typedef struct JabberMessageErrorData
+{
+    char		*Error;
+    unsigned	Code;
+} JabberMessageErrorData;
+
+class JabberMessageError : public Message
+{
+public:
+    JabberMessageError(const char *cfg = NULL);
+    ~JabberMessageError();
+    PROP_UTF8(Error);
+    PROP_ULONG(Code);
+    virtual string save();
+    virtual QString presentation();
+protected:
+    JabberMessageErrorData	data;
+};
+
+typedef struct JabberMessageFileData
+{
+    char		*ID;
+    char		*From;
+    char		*Host;
+    unsigned	Port;
+} JabberMessageFileData;
+
+class JabberFileMessage : public FileMessage
+{
+public:
+    JabberFileMessage(const char *cfg = NULL);
+    ~JabberFileMessage();
+    PROP_STR(ID);
+    PROP_STR(From);
+    PROP_STR(Host);
+    PROP_USHORT(Port);
+protected:
+    JabberMessageFileData	data;
+};
+
+#endif
+
