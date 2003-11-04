4b7db61d880cbb9da0868d686758fe1f8cbec651
shutoff <shutoff@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-11-04 09:48:13 +0000

    Fix ICQ login
    git-svn-id: file:///.data/svn_convert/svn@2815 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- branches/unlabeled-1.7.2/plugins/icq/icqlogin.cpp
+++ branches/unlabeled-1.7.2/plugins/icq/icqlogin.cpp
@@ -130,7 +130,6 @@ void ICQClient::chn_login()
             if (c == '0') c = 0;
             pass[n++] = c;
         }
-
         m_socket->writeBuffer << 0x00000001L;
         m_socket->writeBuffer.tlv(0x0001, uin);
         m_socket->writeBuffer.tlv(0x0002, pass, n);
@@ -139,7 +138,7 @@ void ICQClient::chn_login()
         m_socket->writeBuffer.tlv(0x0017, 0x0014);
         m_socket->writeBuffer.tlv(0x0018, 0x0009);
         m_socket->writeBuffer.tlv(0x0019, 0x0000);
-        m_socket->writeBuffer.tlv(0x001A, 0x04E5);
+        m_socket->writeBuffer.tlv(0x001A, 0x0515);
         m_socket->writeBuffer.tlv(0x0014, 0x0000043DL);
         m_socket->writeBuffer.tlv(0x000f, "en");
         m_socket->writeBuffer.tlv(0x000e, "us");
