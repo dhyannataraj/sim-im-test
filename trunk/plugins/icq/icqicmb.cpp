872525ff768ea1ad52159cc988c311b02d62614f
shutoff <shutoff@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-11-04 09:59:28 +0000

    Fix compile Fil icq login
    git-svn-id: file:///.data/svn_convert/svn@2816 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/plugins/icq/icqicmb.cpp
+++ trunk/plugins/icq/icqicmb.cpp
@@ -1373,7 +1373,7 @@ void ICQClient::processSendQueue()
                         msgBuf.pack(cc[j].alias);
                     m_send.id.id_l = rand();
                     m_send.id.id_h = rand();
-                    sendType2(m_send.screen.c_str(), msgBuf, m_send.id, CAP_AIM_BUDDYLIST, false, false, false);
+                    sendType2(m_send.screen.c_str(), msgBuf, m_send.id, CAP_AIM_BUDDYLIST, false, 0);
                     return;
                 }
             case MessageUrl:{
@@ -1571,7 +1571,7 @@ void ICQClient::processSendQueue()
             msgBuf.pack((unsigned long)(this->data.owner.Port));
             msgBuf.pack((unsigned short)8);
             msgBuf.pack((unsigned long)m_nMsgSequence);
-            sendType2(m_send.screen.c_str(), msgBuf, m_send.id, CAP_DIRECT, false, false, false);
+            sendType2(m_send.screen.c_str(), msgBuf, m_send.id, CAP_DIRECT, false, 0);
             return;
         }
         if (m_send.flags == PLUGIN_AR){
