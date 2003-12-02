5ce56917c03bd133484cae4e7abb8ba513fc86c3
shutoff <shutoff@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-12-02 15:42:06 +0000

    Fix login with long password
    git-svn-id: file:///.data/svn_convert/svn@3072 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- branches/unlabeled-1.10.2/plugins/icq/icqlogin.cpp
+++ branches/unlabeled-1.10.2/plugins/icq/icqlogin.cpp
@@ -119,20 +119,9 @@ void ICQClient::chn_login()
         sprintf(uin, "%lu", data.owner.Uin);
 
         flap(ICQ_CHNxNEW);
-        unsigned short n = 0;
-        char pass[16];
-        for (const char *p = pswd.c_str(); *p && n < 16; p++){
-            if (*p != '\\'){
-                pass[n++] = *p;
-                continue;
-            }
-            char c = *(++p);
-            if (c == '0') c = 0;
-            pass[n++] = c;
-        }
         m_socket->writeBuffer << 0x00000001L;
         m_socket->writeBuffer.tlv(0x0001, uin);
-        m_socket->writeBuffer.tlv(0x0002, pass, n);
+        m_socket->writeBuffer.tlv(0x0002, pswd.c_str(), pswd.length());
         m_socket->writeBuffer.tlv(0x0003, "3ICQ Inc. - Product of ICQ (TM).2003b.5.56.1.3916.85");
         m_socket->writeBuffer.tlv(0x0016, 0x010A);
         m_socket->writeBuffer.tlv(0x0017, 0x0002);
