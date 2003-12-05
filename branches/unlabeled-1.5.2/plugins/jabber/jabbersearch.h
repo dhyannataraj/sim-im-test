b4e8249672eea55d3d77da9749040dc049d76680
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-12-05 09:34:03 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.
    git-svn-id: file:///.data/svn_convert/svn@3102 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.2/plugins/jabber/jabbersearch.h
@@ -0,0 +1,55 @@
+/***************************************************************************
+                          jabbersearch.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _JABBERSEARCH_H
+#define _JABBERSEARCH_H
+
+#include "simapi.h"
+#include "stl.h"
+#include "qchildwidget.h"
+
+class JabberClient;
+
+class QGridLayout;
+
+class JabberSearch : public QChildWidget
+{
+    Q_OBJECT
+public:
+    JabberSearch(QWidget *receiver, JabberClient *client, const char *jid, const QString &name);
+    bool canSearch();
+    QString condition();
+    const char *id() { return m_jid.c_str(); }
+    void addWidget(struct JabberAgentInfo *data);
+    JabberClient	*m_client;
+    string			m_jid;
+protected slots:
+    void setSize();
+protected:
+    QString			i18(const char *text);
+    QGridLayout		*lay;
+    QString			m_name;
+    QWidget			*m_receiver;
+    string			m_key;
+    unsigned		m_nPos;
+    bool			m_bDirty;
+    bool			m_bXData;
+    list<QWidget*>	m_required;
+};
+
+#endif
+
