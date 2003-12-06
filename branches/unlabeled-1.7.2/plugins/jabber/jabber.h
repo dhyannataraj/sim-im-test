aca2bb1022c77a938583cf2f73ce77f2b272cbe5
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2003-12-06 10:53:59 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.7.2'.
    git-svn-id: file:///.data/svn_convert/svn@3119 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.7.2/plugins/jabber/jabber.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+                          jabber.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _JABBER_H
+#define _JABBER_H
+
+#include "simapi.h"
+
+class JabberProtocol : public Protocol
+{
+public:
+    JabberProtocol(Plugin *plugin);
+    ~JabberProtocol();
+    Client	*createClient(const char *cfg);
+    const CommandDef *description();
+    const CommandDef *statusList();
+    virtual const DataDef *userDataDef();
+};
+
+class JabberPlugin : public Plugin
+{
+public:
+    JabberPlugin(unsigned base);
+    virtual ~JabberPlugin();
+    unsigned JabberPacket;
+    unsigned EventAgentFound;
+    unsigned EventAgentInfo;
+    unsigned EventAgentRegister;
+    unsigned EventSearch;
+    unsigned EventSearchDone;
+    unsigned EventDiscoItem;
+    unsigned CmdSendMessage;
+    unsigned CmdGroups;
+    unsigned CmdBrowser;
+    unsigned CmdBack;
+    unsigned CmdForward;
+    unsigned CmdUrl;
+    unsigned CmdInfo;
+    unsigned CmdSearch;
+    unsigned CmdRegister;
+    unsigned CmdNode;
+    unsigned CmdConfigure;
+    unsigned MenuSearchResult;
+    unsigned MenuGroups;
+    unsigned BarBrowser;
+    void registerMessages();
+    void unregisterMessages();
+protected:
+    Protocol *m_protocol;
+};
+
+#endif
+
