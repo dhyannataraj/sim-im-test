1dbb20e83fb30a026808d7b952a6497861f9cd85
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-01-08 02:56:13 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.2.2'.
    git-svn-id: file:///.data/svn_convert/svn@3364 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.2.2/plugins/livejournal/livejournalcfg.cpp
@@ -0,0 +1,84 @@
+/***************************************************************************
+                          livejournalcfg.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "livejournalcfg.h"
+#include "livejournal.h"
+#include "linklabel.h"
+
+#include <qcheckbox.h>
+#include <qlineedit.h>
+#include <qspinbox.h>
+#include <qtimer.h>
+
+LiveJournalCfg::LiveJournalCfg(QWidget *parent, LiveJournalClient *client, bool bConfig)
+        : LiveJournalCfgBase(parent)
+{
+    m_client = client;
+    m_bConfig = bConfig;
+    if (client->data.owner.User)
+        edtName->setText(QString::fromUtf8(client->data.owner.User));
+    if (bConfig){
+        edtPassword->setText(client->getPassword());
+        lblLnk->setText(i18n("Register new user"));
+        lblLnk->setUrl("http://www.livejournal.com/create.bml");
+    }else{
+        edtName->setReadOnly(true);
+        edtPassword->hide();
+        lblPassword->hide();
+    }
+    edtServer->setText(client->getServer());
+    edtPath->setText(client->getURL());
+    edtPort->setValue(client->getPort());
+    edtInterval->setValue(client->getInterval());
+    chkFastServer->setChecked(client->getFastServer());
+    connect(edtName, SIGNAL(textChanged(const QString&)), this, SLOT(changed(const QString&)));
+    connect(edtPassword, SIGNAL(textChanged(const QString&)), this, SLOT(changed(const QString&)));
+    changed("");
+    QTimer::singleShot(0, this, SLOT(changed()));
+}
+
+void LiveJournalCfg::changed(const QString&)
+{
+    changed();
+}
+
+void LiveJournalCfg::changed()
+{
+    emit okEnabled(!edtName->text().isEmpty() && !edtPassword->text().isEmpty());
+}
+
+void LiveJournalCfg::apply()
+{
+    if (m_bConfig){
+        set_str(&m_client->data.owner.User, edtName->text().utf8());
+        m_client->setPassword(edtPassword->text().utf8());
+    }
+    m_client->setServer(edtServer->text().latin1());
+    m_client->setURL(edtPath->text().latin1());
+    m_client->setPort(atol(edtPort->text()));
+    m_client->setInterval(atol(edtInterval->text()));
+    m_client->setFastServer(chkFastServer->isChecked());
+}
+
+void LiveJournalCfg::apply(Client*, void*)
+{
+}
+
+#ifndef WIN32
+#include "livejournalcfg.moc"
+#endif
+
---
+++ branches/unlabeled-1.2.2/plugins/sms/gsm_ta.h
@@ -0,0 +1,131 @@
+/***************************************************************************
+                          gsm_ta.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _GSM_TA_H
+#define _GSM_TA_H
+
+#include "simapi.h"
+#include "stl.h"
+
+class SerialPort;
+class QTimer;
+
+typedef struct OpInfo
+{
+    unsigned	oper;
+    string		param;
+} OpInfo;
+
+class Phonebook
+{
+public:
+	Phonebook();
+	unsigned m_size;
+	unsigned m_used;
+	unsigned m_numberSize;
+	unsigned m_nameSize;
+	vector<bool> m_entries;
+};
+
+class GsmTA : public QObject
+{
+    Q_OBJECT
+public:
+    GsmTA(QObject *parent);
+    ~GsmTA();
+    bool open(const char *device, int baudrate, bool bXonXoff, const char *init);
+    string model();
+	string oper();
+    void getPhoneBook();
+    void setPhoneBookEntry(unsigned index, const QString &phone, const QString &name);
+signals:
+    void init_done();
+    void error();
+	void phoneCall(const QString &phone);
+    void phonebookEntry(int index, int type, const QString &phone, const QString &name);
+	void quality(unsigned);
+	void charge(bool, unsigned);
+protected slots:
+    void write_ready();
+    void read_ready();
+    void port_error();
+    void ping();
+protected:
+    enum State
+    {
+        None,
+        Open,
+        Init,
+        Init2,
+        Init3,
+        Init4,
+		Init5,
+		Init6,
+		Init7,
+        Info1,
+        Info2,
+        Info3,
+        Info4,
+        Charset1,
+        Charset2,
+        OpInfo1,
+        OpInfo2,
+        Connected,
+        Ping,
+		PhoneBook,
+        PhoneBook1,
+        PhoneBook2,
+        PhoneBook3,
+        PhoneBookStore
+    };
+    unsigned m_tries;
+    void at(const char *str, unsigned timeout=10000);
+    bool isOK(const char *answer);
+    bool isError(const char *answer);
+    bool isChatOK(const char *amswer, const char *response = NULL,
+                  bool bIgnoreErrors = false, bool bAcceptEmptyResponse = false);
+    bool isChatResponse(const char *answer, const char *response = NULL,
+                        bool bIgnoreErrors = false);
+	bool isIncoming(const char *answer);
+    bool matchResponse(string &answer, const char *responseToMatch);
+    void processQueue();
+    void parseEntriesList(const char *answ);
+    void parseEntry(const char *answ);
+    void getNextEntry();
+    string normalize(const char *answ);
+    string gsmToLatin1(const char *str);
+    string latin1ToGsm(const char *str);
+    State			m_state;
+    string			m_initString;
+    string			m_cmd;
+    string			m_manufacturer;
+    string			m_model;
+    string			m_revision;
+    string			m_serialNumber;
+    string			m_operator;
+    string			m_response;
+    string			m_charset;
+    list<OpInfo>	m_queue;
+	Phonebook		m_books[2];
+	Phonebook		*m_book;
+	unsigned		m_bookType;
+    QTimer			*m_timer;
+    SerialPort		*m_port;
+};
+
+#endif
+
