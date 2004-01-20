60fe3188d7d9430597b76ded8bd3a5fb036f1ab5
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-01-20 17:17:41 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.6.2'.
    git-svn-id: file:///.data/svn_convert/svn@3529 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.6.2/plugins/floaty/floaty.cpp
@@ -0,0 +1,303 @@
+/***************************************************************************
+                          floaty.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "floaty.h"
+#include "floatywnd.h"
+#include "simapi.h"
+
+#include "core.h"
+
+#include "xpm/floating.xpm"
+
+#include <qpopupmenu.h>
+#include <qapplication.h>
+#include <qwidgetlist.h>
+#include <qtimer.h>
+
+Plugin *createFloatyPlugin(unsigned base, bool, const char*)
+{
+    FloatyPlugin *plugin = new FloatyPlugin(base);
+    if (plugin->core == NULL){
+        delete plugin;
+        return NULL;
+    }
+    return plugin;
+}
+
+static PluginInfo info =
+    {
+        I18N_NOOP("Floaty"),
+        I18N_NOOP("Plugin provides floating windows for contacts"),
+        VERSION,
+        createFloatyPlugin,
+        PLUGIN_DEFAULT
+    };
+
+EXPORT_PROC PluginInfo* GetPluginInfo()
+{
+    return &info;
+}
+
+/*
+typedef struct FloatyUserData
+{
+	unsigned	Floaty;
+	unsigned	X;
+	unsigned	Y;
+} FloatyUserData;
+*/
+
+static DataDef floatyUserData[] =
+    {
+        { "Floaty", DATA_BOOL, 1, 0 },
+        { "FloatyPosition", DATA_ULONG, 2, 0 },
+        { NULL, 0, 0, 0 }
+    };
+
+FloatyPlugin::FloatyPlugin(unsigned base)
+        : Plugin(base)
+{
+    CmdFloaty = registerType();
+    user_data_id = getContacts()->registerUserData(info.title, floatyUserData);
+
+    IconDef icon;
+    icon.name = "floating";
+    icon.xpm = floating;
+    icon.isSystem = false;
+
+    m_bBlink = false;
+    unreadTimer = new QTimer(this);
+    connect(unreadTimer, SIGNAL(timeout()), this, SLOT(unreadBlink()));
+
+    Event eIcon(EventAddIcon, &icon);
+    eIcon.process();
+
+    Command cmd;
+    cmd->id		  = CmdFloaty;
+    cmd->text	  = I18N_NOOP("Floating on");
+    cmd->icon	  = "floating";
+    cmd->menu_id  = MenuContact;
+    cmd->menu_grp = 0xB000;
+    cmd->flags	  = COMMAND_CHECK_STATE;
+    Event e(EventCommandCreate, cmd);
+    e.process();
+
+    Event ePlugin(EventGetPluginInfo, (void*)"_core");
+    pluginInfo *info = (pluginInfo*)(ePlugin.process());
+    core = static_cast<CorePlugin*>(info->plugin);
+}
+
+FloatyPlugin::~FloatyPlugin()
+{
+    QWidgetList *list = QApplication::topLevelWidgets();
+    QWidgetListIt it(*list);
+    QWidget * w;
+    while ( (w=it.current()) != 0 ) {
+        if (w->inherits("FloatyWnd"))
+            delete w;
+        ++it;
+    }
+    delete list;
+    Event e(EventCommandRemove, (void*)CmdFloaty);
+    e.process();
+    getContacts()->unregisterUserData(user_data_id);
+}
+
+FloatyWnd *FloatyPlugin::findFloaty(unsigned id)
+{
+    QWidgetList *list = QApplication::topLevelWidgets();
+    QWidgetListIt it(*list);
+    QWidget * w;
+    while ((w = it.current()) != NULL) {
+        if (w->inherits("FloatyWnd")){
+            FloatyWnd *wnd = static_cast<FloatyWnd*>(w);
+            if (wnd->id() == id)
+                break;
+        }
+        ++it;
+    }
+    delete list;
+    if (w)
+        return static_cast<FloatyWnd*>(w);
+    return NULL;
+}
+
+void *FloatyPlugin::processEvent(Event *e)
+{
+    switch (e->type()){
+    case EventInit:{
+            Contact *contact;
+            ContactList::ContactIterator it;
+            while ((contact = ++it) != NULL){
+                FloatyUserData *data = (FloatyUserData*)(contact->userData.getUserData(user_data_id, false));
+                if (data == NULL)
+                    continue;
+                FloatyWnd *wnd = new FloatyWnd(this, contact->id());
+                wnd->move(data->X.value, data->Y.value);
+                wnd->show();
+            }
+            break;
+        }
+    case EventCheckState:{
+            CommandDef *cmd = (CommandDef*)(e->param());
+            if (cmd->id == CmdFloaty){
+                Contact *contact = getContacts()->contact((unsigned)(cmd->param));
+                if (contact){
+                    FloatyUserData *data = (FloatyUserData*)(contact->userData.getUserData(user_data_id, false));
+                    if (data){
+                        cmd->text = I18N_NOOP("Floating off");
+                        cmd->flags |= COMMAND_CHECKED;
+                    }else{
+                        cmd->text = I18N_NOOP("Floating on");
+                        cmd->flags &= ~COMMAND_CHECKED;
+                    }
+                }
+                return e->param();
+            }
+            break;
+        }
+    case EventCommandExec:{
+            CommandDef *cmd = (CommandDef*)(e->param());
+            if (cmd->id == CmdFloaty){
+                Contact *contact = getContacts()->contact((unsigned)(cmd->param));
+                if (contact){
+                    FloatyUserData *data = (FloatyUserData*)(contact->userData.getUserData(user_data_id, false));
+                    if (data){
+                        FloatyWnd *wnd = findFloaty(contact->id());
+                        if (wnd)
+                            delete wnd;
+                        contact->userData.freeUserData(user_data_id);
+                    }else{
+                        data = (FloatyUserData*)(contact->userData.getUserData(user_data_id, true));
+                        FloatyWnd *wnd = new FloatyWnd(this, (unsigned)(cmd->param));
+                        wnd->move(0, 0);
+                        wnd->show();
+                    }
+                }
+                return e->param();
+            }
+            break;
+        }
+    case EventMessageDeleted:
+    case EventMessageRead:
+    case EventMessageReceived:{
+            Message *msg = (Message*)(e->param());
+            FloatyWnd *wnd = findFloaty(msg->contact());
+            if (wnd){
+                wnd->init();
+                wnd->repaint();
+            }
+            break;
+        }
+    case EventContactClient:
+    case EventContactStatus:
+    case EventContactChanged:{
+            Contact *contact = (Contact*)(e->param());
+            FloatyWnd *wnd = findFloaty(contact->id());
+            if (wnd){
+                wnd->init();
+                wnd->repaint();
+            }
+            break;
+        }
+    case EventContactDeleted:{
+            Contact *contact = (Contact*)(e->param());
+            FloatyWnd *wnd = findFloaty(contact->id());
+            if (wnd)
+                delete wnd;
+            break;
+        }
+    case EventRepaintView:{
+            QWidgetList *list = QApplication::topLevelWidgets();
+            QWidgetListIt it(*list);
+            QWidget * w;
+            while ((w = it.current()) != NULL) {
+                if (w->inherits("FloatyWnd")){
+                    FloatyWnd *wnd = static_cast<FloatyWnd*>(w);
+                    wnd->init();
+                    wnd->repaint();
+                }
+                ++it;
+            }
+            delete list;
+            break;
+        }
+    }
+    return NULL;
+}
+
+void FloatyPlugin::showPopup()
+{
+    ProcessMenuParam mp;
+    mp.id    = MenuContact;
+    mp.param = (void*)(popupId);
+    mp.key	 = 0;
+    Event eMenu(EventProcessMenu, &mp);
+    QPopupMenu *menu = (QPopupMenu*)eMenu.process();
+    menu->popup(popupPos);
+}
+
+void FloatyPlugin::startBlink()
+{
+    if (unreadTimer->isActive())
+        return;
+    m_bBlink = true;
+    unreadTimer->start(800);
+}
+
+void FloatyPlugin::unreadBlink()
+{
+    m_bBlink = !m_bBlink;
+    QWidgetList *list = QApplication::topLevelWidgets();
+    QWidgetListIt it(*list);
+    QWidget * w;
+    while ((w = it.current()) != NULL) {
+        if (w->inherits("FloatyWnd")){
+            FloatyWnd *wnd = static_cast<FloatyWnd*>(w);
+            wnd->repaint();
+        }
+        ++it;
+    }
+    delete list;
+}
+
+#ifdef WIN32
+
+#include <windows.h>
+
+/**
+ * DLL's entry point
+ **/
+int WINAPI DllMain(HINSTANCE, DWORD, LPVOID)
+{
+    return TRUE;
+}
+
+/**
+ * This is to prevent the CRT from loading, thus making this a smaller
+ * and faster dll.
+ **/
+extern "C" BOOL __stdcall _DllMainCRTStartup( HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    return DllMain( hinstDLL, fdwReason, lpvReserved );
+}
+
+#endif
+
+#ifndef WIN32
+#include "floaty.moc"
+#endif
+
---
+++ branches/unlabeled-1.6.2/plugins/jabber/jabberinfo.cpp
@@ -0,0 +1,162 @@
+/***************************************************************************
+                          jabberinfo.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "simapi.h"
+#include "jabberinfo.h"
+#include "jabberclient.h"
+#include "datepicker.h"
+
+#include <qmultilineedit.h>
+#include <qlineedit.h>
+#include <qstringlist.h>
+#include <qcombobox.h>
+#include <qpushbutton.h>
+#include <qpixmap.h>
+#include <qlabel.h>
+
+JabberInfo::JabberInfo(QWidget *parent, struct JabberUserData *data, JabberClient *client)
+        : JabberInfoBase(parent)
+{
+    m_client  = client;
+    m_data    = data;
+    btnUrl->setPixmap(Pict("home"));
+    connect(btnUrl, SIGNAL(clicked()), this, SLOT(goUrl()));
+    edtOnline->setReadOnly(true);
+    edtNA->setReadOnly(true);
+    edtResource->setReadOnly(true);
+    edtID->setReadOnly(true);
+    if (m_data){
+        edtFirstName->setReadOnly(true);
+        edtNick->setReadOnly(true);
+        disableWidget(edtDate);
+        edtUrl->setReadOnly(true);
+        edtAutoReply->setReadOnly(true);
+    }else{
+        connect(edtUrl, SIGNAL(textChanged(const QString&)), this, SLOT(urlChanged(const QString&)));
+        edtAutoReply->hide();
+    }
+    fill();
+}
+
+void JabberInfo::apply()
+{
+}
+
+void *JabberInfo::processEvent(Event *e)
+{
+    if ((e->type() == EventMessageReceived) && m_data){
+        Message *msg = (Message*)(e->param());
+        if ((msg->type() == MessageStatus) && (m_client->dataName(m_data) == msg->client()))
+            fill();
+    }
+    if (e->type() == EventContactChanged){
+        Contact *contact = (Contact*)(e->param());
+        if (contact->clientData.have(m_data))
+            fill();
+    }
+    if ((e->type() == EventClientChanged) && (m_data == 0)){
+        Client *client = (Client*)(e->param());
+        if (client == m_client)
+            fill();
+    }
+    return NULL;
+}
+
+void JabberInfo::fill()
+{
+    JabberUserData *data = m_data;
+    if (data == NULL) data = &m_client->data.owner;
+    edtID->setText(QString::fromUtf8(data->ID.ptr));
+    edtFirstName->setText(data->FirstName.ptr ? QString::fromUtf8(data->FirstName.ptr) : QString(""));
+    edtNick->setText(data->Nick.ptr ? QString::fromUtf8(data->Nick.ptr) : QString(""));
+    edtDate->setText(data->Bday.ptr ? QString::fromUtf8(data->Bday.ptr) : QString(""));
+    edtUrl->setText(data->Url.ptr ? QString::fromUtf8(data->Url.ptr) : QString(""));
+    urlChanged(edtUrl->text());
+    int current = 0;
+    const char *text = NULL;
+    unsigned status = m_data ? m_data->Status.value : m_client->getStatus();
+    for (const CommandDef *cmd = m_client->protocol()->statusList(); cmd->id; cmd++){
+        if (cmd->flags & COMMAND_CHECK_STATE)
+            continue;
+        if (status == cmd->id){
+            current = cmbStatus->count();
+            text = cmd->text;
+        }
+        cmbStatus->insertItem(Pict(cmd->icon), i18n(cmd->text));
+    }
+    cmbStatus->setCurrentItem(current);
+    disableWidget(cmbStatus);
+    if (status == STATUS_OFFLINE){
+        lblOnline->setText(i18n("Last online") + ":");
+        edtOnline->setText(formatDateTime(data->StatusTime.value));
+        lblNA->hide();
+        edtNA->hide();
+    }else{
+        if (data->OnlineTime.value){
+            edtOnline->setText(formatDateTime(data->OnlineTime.value));
+        }else{
+            lblOnline->hide();
+            edtOnline->hide();
+        }
+        if ((status == STATUS_ONLINE) || (text == NULL)){
+            lblNA->hide();
+            edtNA->hide();
+        }else{
+            lblNA->setText(i18n(text));
+            edtNA->setText(formatDateTime(data->StatusTime.value));
+        }
+    }
+    if ((status != STATUS_ONLINE) && (status != STATUS_OFFLINE) && m_data){
+        QString autoReply;
+        if (m_data->AutoReply.ptr)
+            autoReply = QString::fromUtf8(m_data->AutoReply.ptr);
+        edtAutoReply->setText(autoReply);
+    }else{
+        edtAutoReply->hide();
+    }
+    edtResource->setText(data->Resource.ptr ? QString::fromUtf8(data->Resource.ptr) : QString(""));
+}
+
+void JabberInfo::apply(Client *client, void *_data)
+{
+    if (client != m_client)
+        return;
+    JabberUserData *data = (JabberUserData*)_data;
+    set_str(&data->FirstName.ptr, edtFirstName->text().utf8());
+    set_str(&data->Nick.ptr, edtNick->text().utf8());
+    set_str(&data->Bday.ptr, edtDate->text().utf8());
+    set_str(&data->Url.ptr, edtUrl->text().utf8());
+}
+
+void JabberInfo::goUrl()
+{
+    QString url = edtUrl->text();
+    if (url.isEmpty())
+        return;
+    Event e(EventGoURL, (void*)(const char*)(url.local8Bit()));
+    e.process();
+}
+
+void JabberInfo::urlChanged(const QString &text)
+{
+    btnUrl->setEnabled(!text.isEmpty());
+}
+
+#ifndef WIN32
+#include "jabberinfo.moc"
+#endif
+
