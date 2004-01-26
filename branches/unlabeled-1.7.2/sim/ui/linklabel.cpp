e520007a17597f0ba8c60a2b0a31a891a5125206
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-01-26 08:51:00 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.7.2'.
    git-svn-id: file:///.data/svn_convert/svn@3581 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.7.2/sim/ui/linklabel.cpp
@@ -0,0 +1,118 @@
+/***************************************************************************
+                          linklabel.cpp  -  description
+                             -------------------
+    begin                : Sun Mar 24 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <qsimplerichtext.h>
+
+#include "linklabel.h"
+
+#include <qcursor.h>
+#include <qapplication.h>
+#include <qtooltip.h>
+#include <qstylesheet.h>
+#include <qpainter.h>
+
+LinkLabel::LinkLabel(QWidget *parent, const char *name)
+        : QLabel(parent, name)
+{
+    setCursor(QCursor(PointingHandCursor));
+    QFont f = font();
+    f.setUnderline(true);
+    setFont(f);
+}
+
+void LinkLabel::setUrl(const QString &url)
+{
+    m_url = url;
+}
+
+void LinkLabel::mouseReleaseEvent(QMouseEvent * e)
+{
+    if ((e->button() == LeftButton) && !m_url.isEmpty()){
+        string url;
+        url = m_url.latin1();
+        Event e(EventGoURL, (void*)(url.c_str()));
+        e.process();
+    }
+}
+
+TipLabel::TipLabel(const QString &text)
+        : QLabel(NULL, "toolTipTip", WStyle_StaysOnTop | WStyle_Customize | WStyle_NoBorder | WStyle_Tool | WX11BypassWM)
+{
+    setMargin(3);
+    setAutoMask( FALSE );
+    setFrameStyle(QFrame::Plain | QFrame::Box);
+    setLineWidth(1);
+    polish();
+    m_text = text;
+    setPalette(QToolTip::palette());
+}
+
+TipLabel::~TipLabel()
+{
+    emit finished();
+}
+
+extern QMimeSourceFactory *_factory;
+
+void TipLabel::setText(const QString &text)
+{
+    m_text = text;
+}
+
+void TipLabel::show(const QRect &tipRect, bool bState)
+{
+    QSimpleRichText richText(m_text, font(), "", QStyleSheet::defaultSheet(), _factory, -1, Qt::blue, false);
+    richText.adjustSize();
+    QSize s(richText.widthUsed() + 8, richText.height() + 8);
+    resize(s.width(), s.height());
+    QRect rc = screenGeometry();
+    int x = tipRect.left() + tipRect.width() / 2 - width();
+    if (x < 0)
+        x = tipRect.left() + tipRect.width() / 2;
+    if (x + width() > rc.width() - 2)
+        x = rc.width() - 2 - width();
+    int y = 0;
+    if (bState){
+        y = tipRect.top() - 4 - height();
+        if (y < 0)
+            bState = false;
+    }
+    if (!bState)
+        y = tipRect.top() + tipRect.height() + 4;
+    if (y + height() > rc.height())
+        y = tipRect.top() - 4 - height();
+    if (y < 0)
+        y = tipRect.top() + tipRect.height() + 4;
+    move(x, y);
+    QLabel::show();
+}
+
+void TipLabel::drawContents(QPainter *p)
+{
+    QSimpleRichText richText(m_text, font(), "", QStyleSheet::defaultSheet(), _factory, -1, Qt::blue, false);
+    richText.adjustSize();
+#if QT_VERSION < 0x300
+    richText.draw(p, 4, 4, QRegion(0, 0, width(), height()), QToolTip::palette());
+#else
+    richText.draw(p, 4, 4, QRect(0, 0, width(), height()), QToolTip::palette().active());
+#endif
+}
+
+#ifndef WIN32
+#include "linklabel.moc"
+#endif
+
