ca0ab980297c031dfb4486286b56d759a56ee65d
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-02-02 00:55:24 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.
    git-svn-id: file:///.data/svn_convert/svn@3609 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.2/sim/ui/linklabel.h
@@ -0,0 +1,51 @@
+/***************************************************************************
+                          linklabel.h  -  description
+                             -------------------
+    begin                : Sun Mar 24 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _LINKLABEL_H
+#define _LINKLABEL_H
+
+#include "simapi.h"
+#include <qlabel.h>
+
+class UI_EXPORT LinkLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    LinkLabel(QWidget *parent = NULL, const char *name = NULL);
+    void setUrl(const QString &url);
+protected:
+    virtual void mouseReleaseEvent(QMouseEvent * e);
+    QString m_url;
+};
+
+class UI_EXPORT TipLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    TipLabel(const QString &text);
+    ~TipLabel();
+    void setText(const QString &text);
+    void show(const QRect &tipRect, bool bState = false);
+signals:
+    void finished();
+protected:
+    virtual void drawContents(QPainter*);
+    QString m_text;
+};
+
+#endif
+
