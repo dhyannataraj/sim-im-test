21cfe79f830b5bbe116a280bf00a28f9aa360afa
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-03-12 08:30:11 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.2'.
    git-svn-id: file:///.data/svn_convert/svn@3968 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.4.2/plugins/weather/weather.h
@@ -0,0 +1,147 @@
+/***************************************************************************
+                          weather.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _WEATHER_H
+#define _WEATHER_H
+
+#include "simapi.h"
+#include <libxml/parser.h>
+
+class QToolBar;
+
+typedef struct WeatherData
+{
+    Data	ID;
+    Data	Location;
+    Data	Time;
+    Data	ForecastTime;
+    Data	Forecast;
+    Data	Text;
+    Data	Tip;
+    Data	ForecastTip;
+    Data	Units;
+    Data	bar[7];
+    Data	Updated;
+    Data	Temperature;
+    Data	FeelsLike;
+    Data	DewPoint;
+    Data	Humidity;
+    Data	Pressure;
+    Data	PressureD;
+    Data	Conditions;
+    Data	Wind;
+    Data	Wind_speed;
+    Data	WindGust;
+    Data	Visibility;
+    Data	Sun_raise;
+    Data	Sun_set;
+    Data	Icon;
+    Data	UT;
+    Data	UP;
+    Data	US;
+    Data	UD;
+    Data	Day;
+    Data	WDay;
+    Data	MinT;
+    Data	MaxT;
+    Data	DayIcon;
+    Data	DayConditions;
+} WeatherData;
+
+class WeatherPlugin : public QObject, public Plugin, public EventReceiver
+{
+    Q_OBJECT
+public:
+    WeatherPlugin(unsigned, bool, const char*);
+    virtual ~WeatherPlugin();
+    PROP_STR(ID);
+    PROP_STR(Location);
+    PROP_ULONG(Time);
+    PROP_ULONG(ForecastTime);
+    PROP_ULONG(Forecast);
+    PROP_UTF8(Text);
+    PROP_UTF8(Tip);
+    PROP_UTF8(ForecastTip);
+    PROP_BOOL(Units);
+    PROP_STR(Updated);
+    PROP_LONG(Temperature);
+    PROP_LONG(FeelsLike);
+    PROP_LONG(DewPoint);
+    PROP_LONG(Humidity);
+    PROP_LONG(Pressure);
+    PROP_STR(PressureD);
+    PROP_STR(Conditions);
+    PROP_STR(Wind);
+    PROP_LONG(Wind_speed);
+    PROP_LONG(WindGust);
+    PROP_STR(Visibility);
+    PROP_STR(Sun_raise);
+    PROP_STR(Sun_set);
+    PROP_ULONG(Icon);
+    PROP_STR(UT);
+    PROP_STR(US);
+    PROP_STR(UP);
+    PROP_STR(UD);
+    PROP_STRLIST(Day);
+    PROP_STRLIST(WDay);
+    PROP_STRLIST(MinT);
+    PROP_STRLIST(MaxT);
+    PROP_STRLIST(DayIcon);
+    PROP_STRLIST(DayConditions);
+    QString getButtonText();
+    QString getTipText();
+    QString getForecastText();
+    void updateButton();
+    void showBar();
+    void hideBar();
+    unsigned EventWeather;
+    QToolBar *m_bar;
+protected slots:
+    void timeout();
+    void barDestroyed();
+protected:
+    QString replace(const QString&);
+    QString forecastReplace(const QString&);
+    unsigned BarWeather;
+    unsigned CmdWeather;
+    unsigned m_fetch_id;
+    string m_data;
+    bool   m_bData;
+    bool   m_bBar;
+    bool   m_bWind;
+    bool   m_bUv;
+    bool   m_bForecast;
+    bool   m_bCC;
+    unsigned m_day;
+    string getConfig();
+    bool isDay();
+    bool parseTime(const char *str, int &h, int &m);
+    virtual QWidget *createConfigWindow(QWidget *parent);
+    void *processEvent(Event*);
+    WeatherData data;
+    xmlSAXHandler		m_handler;
+    xmlParserCtxtPtr	m_context;
+    void		element_start(const char *el, const char **attr);
+    void		element_end(const char *el);
+    void		char_data(const char *str, int len);
+    static void p_element_start(void *data, const xmlChar *el, const xmlChar **attr);
+    static void p_element_end(void *data, const xmlChar *el);
+    static void p_char_data(void *data, const xmlChar *str, int len);
+};
+
+#endif
+
---
+++ branches/unlabeled-1.4.2/plugins/weather/weathercfg.h
@@ -0,0 +1,62 @@
+/***************************************************************************
+                          weathercfg.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _WEATHERCFG_H
+#define _WEATHERCFG_H
+
+#include "simapi.h"
+#include "weathercfgbase.h"
+
+#include <libxml/parser.h>
+#include "stl.h"
+
+class WeatherPlugin;
+class WIfaceCfg;
+
+class WeatherCfg : public WeatherCfgBase, public EventReceiver
+{
+    Q_OBJECT
+public:
+    WeatherCfg(QWidget *parent, WeatherPlugin*);
+    ~WeatherCfg();
+public slots:
+    void apply();
+    void search();
+    void activated(int index);
+    void textChanged(const QString&);
+protected:
+    void *processEvent(Event*);
+    void fill();
+    WeatherPlugin *m_plugin;
+    WIfaceCfg	  *m_iface;
+    unsigned m_fetch_id;
+    string   m_id;
+    string	 m_data;
+    vector<string>		m_ids;
+    vector<string>		m_names;
+    xmlSAXHandler		m_handler;
+    xmlParserCtxtPtr	m_context;
+    void		element_start(const char *el, const char **attr);
+    void		element_end(const char *el);
+    void		char_data(const char *str, int len);
+    static void p_element_start(void *data, const xmlChar *el, const xmlChar **attr);
+    static void p_element_end(void *data, const xmlChar *el);
+    static void p_char_data(void *data, const xmlChar *str, int len);
+};
+
+#endif
+
