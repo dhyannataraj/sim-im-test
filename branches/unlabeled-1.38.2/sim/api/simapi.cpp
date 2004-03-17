681804a6632470751cc2f0d2fe3defbf10911e13
shutoff <shutoff@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-03-17 05:58:44 +0000

    Fix compile
    git-svn-id: file:///.data/svn_convert/svn@4046 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- branches/unlabeled-1.38.2/sim/api/simapi.cpp
+++ branches/unlabeled-1.38.2/sim/api/simapi.cpp
@@ -621,11 +621,41 @@ EXPORT void disableWidget(QWidget *w)
     w->setEnabled(false);
 }
 
+const unsigned char NOP = 172;
+
+static unsigned char gsmToLatin1Table[] =
+{
+        '@', 163, '$', 165, 232, 233, 249, 236,
+        242, 199,  10, 216, 248,  13, 197, 229,
+        NOP, '_', NOP, NOP, NOP, NOP, NOP, NOP,
+        NOP, NOP, NOP, NOP, 198, 230, 223, 201,
+        ' ', '!', '"', '#', 164, '%', '&', '\'',
+        '(', ')', '*', '+', ',', '-', '.', '/',
+        '0', '1', '2', '3', '4', '5', '6', '7',
+        '8', '9', ':', ';', '<', '=', '>', '?',
+        161, 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+        'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+        'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+        'X', 'Y', 'Z', 196, 214, 209, 220, 167,
+        191, 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+        'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+        'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+        'x', 'y', 'z', 228, 246, 241, 252, 224
+};
+
 EXPORT bool isLatin(const QString &str)
 {
     for (int i = 0; i < (int)str.length(); i++){
-        if (str[i].unicode() > 127)
-            return false;
+		unsigned short c = str[i].unicode();
+		if ((c > 255) || (c == NOP))
+			return false;
+		unsigned n;
+		for (n = 0; n < sizeof(gsmToLatin1Table); n++){
+			if (gsmToLatin1Table[n] == c)
+				break;
+		}
+		if (n >= sizeof(gsmToLatin1Table))
+			return false;
     }
     return true;
 }
