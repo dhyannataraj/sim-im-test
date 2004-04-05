95d3ad4402a52015c3e810162869aa5ab3715cca
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-04-05 09:10:16 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.
    git-svn-id: file:///.data/svn_convert/svn@4314 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.2/plugins/msn/msn.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+                          msn.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _MSN_H
+#define _MSN_H
+
+#include "simapi.h"
+
+class MSNProtocol : public Protocol
+{
+public:
+    MSNProtocol(Plugin *plugin);
+    ~MSNProtocol();
+    Client	*createClient(const char *cfg);
+    const CommandDef *description();
+    const CommandDef *statusList();
+    virtual const DataDef *userDataDef();
+};
+
+class MSNPlugin : public Plugin
+{
+public:
+    MSNPlugin(unsigned base);
+    virtual ~MSNPlugin();
+    unsigned EventAddOk;
+    unsigned EventAddFail;
+    unsigned MSNPacket;
+    unsigned MSNInitMail;
+    unsigned MSNNewMail;
+protected:
+    Protocol *m_protocol;
+};
+
+#endif
+
