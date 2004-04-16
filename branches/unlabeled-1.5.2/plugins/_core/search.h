f7282c73759deb8eadef40b72a2a416899406db7
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-04-16 00:52:56 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.5.2'.
    git-svn-id: file:///.data/svn_convert/svn@4500 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.5.2/plugins/_core/search.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+                          search.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _SEARCH_H
+#define _SEARCH_H
+
+#include "simapi.h"
+#include "stl.h"
+
+#include "searchbase.h"
+
+class CorePlugin;
+class ListView;
+
+typedef struct ClientWidget
+{
+    Client		*client;
+    QWidget		*widget;
+} ClientWidget;
+
+class SearchDialog : public SearchBase, public EventReceiver
+{
+    Q_OBJECT
+public:
+    SearchDialog();
+    ~SearchDialog();
+public slots:
+    void setAdd(bool bAdd);
+    void clientActivated(int);
+    void aboutToShow(QWidget*);
+    void resultShow(QWidget*);
+    void textChanged(const QString&);
+    void toggled(bool);
+    void addResult(QWidget*);
+    void showResult(QWidget*);
+    void showError(const QString&);
+signals:
+    void finished();
+    void add(unsigned grp_id);
+protected slots:
+    void searchClick();
+    void addGroup(int);
+protected:
+    vector<ClientWidget>	m_widgets;
+    ListView	*m_result;
+    QWidget		*m_current;
+    QWidget		*m_currentResult;
+    void		*processEvent(Event*);
+    void		resizeEvent(QResizeEvent*);
+    void		moveEvent(QMoveEvent*);
+    void		closeEvent(QCloseEvent*);
+    void		fillClients();
+    void		attach(QWidget*);
+    void		detach(QWidget*);
+    bool		checkSearch(QWidget*, bool&);
+    bool		m_bAdd;
+    unsigned	m_id;
+    unsigned	m_result_id;
+};
+
+#endif
+
---
+++ branches/unlabeled-1.5.2/plugins/icq/icqsearch.h
@@ -0,0 +1,59 @@
+/***************************************************************************
+                          icqsearch.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _ICQSEARCH_H
+#define _ICQSEARCH_H
+
+#include "simapi.h"
+#include "icqsearchbase.h"
+
+class ICQClient;
+class AdvSearch;
+class AIMSearch;
+class GroupRadioButton;
+
+class ICQSearch : public ICQSearchBase
+{
+    Q_OBJECT
+public:
+    ICQSearch(ICQClient *client, QWidget *parent);
+    ~ICQSearch();
+signals:
+    void setAdd(bool);
+    void addResult(QWidget*);
+    void showResult(QWidget*);
+protected slots:
+    void advDestroyed();
+    void radioToggled(bool);
+    void advClick();
+    void add(unsigned grp_id);
+protected:
+    void showEvent(QShowEvent*);
+    void setAdv(bool);
+    ICQClient			*m_client;
+    QWidget				*m_adv;
+    GroupRadioButton	*m_btnUin;
+    GroupRadioButton	*m_btnMail;
+    GroupRadioButton	*m_btnName;
+    GroupRadioButton	*m_btnAOL;
+    GroupRadioButton	*m_btnScreen;
+    GroupRadioButton	*m_btnAOL_UIN;
+    bool				m_bAdv;
+};
+
+#endif
+
