29a6ecc617f6724222646a5fa19d59f54d8a29c5
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-04-19 09:18:45 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.4.2'.
    git-svn-id: file:///.data/svn_convert/svn@4552 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.4.2/plugins/msn/msnsearch.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+                          msnsearch.h  -  description
+                             -------------------
+    begin                : Sun Mar 17 2002
+    copyright            : (C) 2002 by Vladimir Shutoff
+    email                : vovan@shutoff.ru
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _MSNSEARCH_H
+#define _MSNSEARCH_H
+
+#include "simapi.h"
+#include "msnsearchbase.h"
+
+class MSNClient;
+class GroupRadioButton;
+
+class MSNSearch : public MSNSearchBase
+{
+    Q_OBJECT
+public:
+    MSNSearch(MSNClient *client, QWidget *parent);
+signals:
+    void setAdd(bool);
+    void showError(const QString&);
+protected slots:
+    void radioToggled(bool);
+    void add(unsigned grp);
+protected:
+    MSNClient *m_client;
+    void showEvent(QShowEvent*);
+    GroupRadioButton	*m_btnMail;
+    GroupRadioButton	*m_btnInfo;
+};
+
+#endif
+
