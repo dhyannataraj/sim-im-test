53dd2780d3a199c2bb3d3292d4dfef7418d9213a
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-05-28 15:09:27 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.3.2'.
    git-svn-id: file:///.data/svn_convert/svn@4807 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/unlabeled-1.3.2/plugins/styles/metal/metal.h
@@ -0,0 +1,93 @@
+/****************************************************************************
+** $Id: metal.h,v 1.3 2004-05-28 15:09:25 chehrlic Exp $
+**
+** Definition of the Metal Style for the themes example
+**
+** Created : 979899
+**
+** Copyright (C) 1997 by Trolltech AS.  All rights reserved.
+**
+** This file is part of an example program for Qt.  This example
+** program may be used, distributed and modified without limitation.
+**
+*****************************************************************************/
+
+#ifndef METAL_H
+#define METAL_H
+
+
+#include <qpalette.h>
+
+#ifndef QT_NO_STYLE_WINDOWS
+
+#include <qwindowsstyle.h>
+#include <qstyleplugin.h>
+
+
+class MetalStyle : public QWindowsStyle
+{
+public:
+    MetalStyle();
+    void polish( QApplication*);
+    void unPolish( QApplication*);
+    void polish( QWidget* );
+    void unPolish( QWidget* );
+
+    void drawPrimitive( PrimitiveElement pe,
+                        QPainter *p,
+                        const QRect &r,
+                        const QColorGroup &cg,
+                        SFlags flags = Style_Default,
+                        const QStyleOption& = QStyleOption::Default) const;
+
+    void drawControl( ControlElement element,
+                      QPainter *p,
+                      const QWidget *widget,
+                      const QRect &r,
+                      const QColorGroup &cg,
+                      SFlags how = Style_Default,
+                      const QStyleOption& = QStyleOption::Default ) const;
+
+    void drawComplexControl( ComplexControl cc,
+                             QPainter *p,
+                             const QWidget *widget,
+                             const QRect &r,
+                             const QColorGroup &cg,
+                             SFlags how = Style_Default,
+                             SCFlags sub = SC_All,
+                             SCFlags subActive = SC_None,
+                             const QStyleOption& = QStyleOption::Default ) const;
+    int pixelMetric( PixelMetric, const QWidget * ) const;
+
+
+private:
+    void drawMetalFrame(  QPainter *p, int x, int y, int w, int h ) const;
+    void drawMetalGradient( QPainter *p, int x, int y, int w, int h,
+                          bool sunken, bool horz, bool flat=FALSE ) const;
+    void drawMetalButton( QPainter *p, int x, int y, int w, int h,
+                          bool sunken, bool horz, bool flat=FALSE ) const;
+    QPalette oldPalette;
+};
+
+class MetalStylePlugin : public QStylePlugin
+{
+public:
+	MetalStylePlugin() {};
+	~MetalStylePlugin() {};
+
+	QStringList keys() const { 
+            return QStringList() << "Metal"; 
+        }
+
+	QStyle* create( const QString& key ) { 
+            if ( key.lower() == "metal" ) 
+                return new MetalStyle();
+            return 0;
+	}
+};
+
+Q_EXPORT_PLUGIN( MetalStylePlugin )
+
+#endif
+
+#endif
