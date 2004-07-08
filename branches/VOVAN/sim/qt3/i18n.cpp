aff8537705dc84f7535be028b49a622d5c5dc65d
(no author) <(no author)@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2004-07-08 08:32:16 +0000

    This commit was manufactured by cvs2svn to create branch 'VOVAN'.
    git-svn-id: file:///.data/svn_convert/svn@5487 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/VOVAN/sim/qt3/i18n.cpp
@@ -0,0 +1,301 @@
+i18n("Hu&e:")
+
+i18n("&Sat:")
+
+i18n("&Val:")
+
+i18n("&Red:")
+
+i18n("&Green:")
+
+i18n("Bl&ue:")
+
+i18n("A&lpha channel:")
+
+i18n("&Basic colors")
+
+i18n("&Custom colors")
+
+i18n("&Define Custom Colors >>")
+
+i18n("OK")
+
+i18n("Cancel")
+
+i18n("&Add To Custom Colors")
+
+i18n("Select color")
+
+i18n("Copy or Move a File")
+
+i18n("Read: %1")
+
+i18n("Write: %1")
+
+i18n("&Cancel")
+
+i18n("All files (*)")
+
+i18n("Name")
+
+i18n("Size")
+
+i18n("Type")
+
+i18n("Date")
+
+i18n("Attributes")
+
+i18n("Look &in:")
+
+i18n("File &name:")
+
+i18n("File &type:")
+
+i18n("Back")
+
+i18n("One directory up")
+
+i18n("Create New Folder")
+
+i18n("List View")
+
+i18n("Detail View")
+
+i18n("Preview File Info")
+
+i18n("Preview File Contents")
+
+i18n("Read-write")
+
+i18n("Read-only")
+
+i18n("Write-only")
+
+i18n("Inaccessible")
+
+i18n("Symlink to File")
+
+i18n("Symlink to Directory")
+
+i18n("Symlink to Special")
+
+i18n("File")
+
+i18n("Dir")
+
+i18n("Special")
+
+i18n("Open")
+
+i18n("Save as")
+
+i18n("&Open")
+
+i18n("&Save")
+
+i18n("&Rename")
+
+i18n("&Delete")
+
+i18n("R&eload")
+
+i18n("Sort by &Name")
+
+i18n("Sort by &Size")
+
+i18n("Sort by &Date")
+
+i18n("&Unsorted")
+
+i18n("Sort")
+
+i18n("Show &hidden files")
+
+i18n("the file")
+
+i18n("the directory")
+
+i18n("the symlink")
+
+i18n("Delete %1")
+
+i18n("<qt>Do you really want to delete %1 \"%2\"?</qt>")
+
+i18n("&Yes")
+
+i18n("&No")
+
+i18n("New Folder 1")
+
+i18n("New Folder")
+
+i18n("New Folder %1")
+
+i18n("Find Directory")
+
+i18n("Directories")
+
+i18n("Save")
+
+i18n("Error")
+
+i18n(
+    "%1\n"
+    "File not found.\n"
+    "Check path and filename.")
+
+i18n("ERROR")
+
+i18n("&Font")
+
+i18n("Font st&yle")
+
+i18n("&Size")
+
+i18n("Effects")
+
+i18n("Stri&keout")
+
+i18n("&Underline")
+
+i18n("&Color")
+
+i18n("Sample")
+
+i18n("Scr&ipt")
+
+i18n("Apply")
+
+i18n("Close")
+
+i18n("Select Font")
+
+i18n("&OK")
+
+i18n(
+    "<h3>About Qt</h3><p>This program uses Qt version %1, a multiplatform C++ GUI "
+    "toolkit from Trolltech. Qt provides single-source portability across Windows "
+    "95/98/NT/2000, Linux, Solaris, HP-UX and many other versions of Unix with "
+    "X11.</p><p>See <tt>http://www.trolltech.com/qt/</tt> for more "
+    "information.</p>")
+
+i18n("Help")
+
+i18n("Defaults")
+
+i18n("< Back")
+
+i18n("Next >")
+
+i18n("Finish")
+
+i18n("Ctrl")
+
+i18n("+")
+
+i18n("Alt")
+
+i18n("Shift")
+
+i18n("F%1")
+
+i18n("ctrl")
+
+i18n("shift")
+
+i18n("alt")
+
+i18n(
+    "Could not rename\n"
+    "%1\n"
+    "to\n"
+    "%2")
+
+i18n(
+    "Could not open\n"
+    "%1")
+
+i18n(
+    "Could not write\n"
+    "%1")
+
+i18n("Operation stopped by the user")
+
+i18n("(unknown)")
+
+i18n("Host %1 found")
+
+i18n("Host found")
+
+i18n("Connected to host %1")
+
+i18n("Connected to host")
+
+i18n("Connection to %1 closed")
+
+i18n("Connection closed")
+
+i18n("Login Incorrect")
+
+i18n("Col %1")
+
+i18n("Row %1")
+
+i18n("Undo")
+
+i18n("Redo")
+
+i18n("Cut")
+
+i18n("Copy")
+
+i18n("Paste")
+
+i18n("Clear")
+
+i18n("Select All")
+
+i18n("&Left")
+
+i18n("&Right")
+
+i18n("&Top")
+
+i18n("&Bottom")
+
+i18n("R&estore")
+
+i18n("&Hide")
+
+i18n("Line Up Toolbars (compact)")
+
+i18n("Line Up Toolbars (normal)")
+
+i18n("Paste special...")
+
+i18n("What's this?")
+
+i18n("&Restore")
+
+i18n("&Move")
+
+i18n("Mi&nimize")
+
+i18n("Ma&ximize")
+
+i18n("&Close")
+
+i18n("&Roll up")
+
+i18n("Minimize")
+
+i18n("Restore Down")
+
+i18n("Maximize")
+
+i18n("Roll up")
+
+i18n("Restore Up")
+
+i18n("Roll down")
