f88a4ea02ae174d91a42532cf7caa3620635160d
icesik <icesik@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2005-09-30 17:43:25 +0000

    correct url from sourceforge to berlios
    git-svn-id: file:///.data/svn_convert/svn@5741 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim-qt4/sim/sim.cpp
+++ trunk/sim-qt4/sim/sim.cpp
@@ -201,8 +201,8 @@ int main(int argc, char *argv[])
                          KAboutData::License_GPL,
                          "Copyright (C) 2002-2004, Vladimir Shutoff",
                          0,
-                         "http://sim-icq.sourceforge.net/",
-                         "sim-icq-main@lists.sourceforge.net");
+                         "http://sim-im.berlios.de/",
+                         "https://lists.berlios.de/mailman/listinfo/sim-im-main");
     aboutData.addAuthor("Vladimir Shutoff",I18N_NOOP("Maintainer"),"vovan@shutoff.ru");
     aboutData.addAuthor("Christian Ehrlicher",I18N_NOOP("Developer"),"Ch.Ehrlicher@gmx.de");
     setAboutData(&aboutData);
