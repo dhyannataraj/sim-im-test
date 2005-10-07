afda4d81b706e8d79e5d7f380e549892b42789ae
fissie <fissie@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2005-10-07 15:23:00 +0000

    Several more unnecessary warnings fixed, I really, really hate them.
    git-svn-id: file:///.data/svn_convert/svn@5748 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim/plugins/yahoo/yahooauth.cpp
+++ trunk/sim/plugins/yahoo/yahooauth.cpp
@@ -941,12 +941,12 @@ void YahooClient::process_auth(const char *method, const char *seed, const char 
     SHA_CTX				ctx1;
     SHA_CTX				ctx2;
 
-    char				*alphabet1			= "FBZDWAGHrJTLMNOPpRSKUVEXYChImkwQ";
-    char				*alphabet2			= "F0E1D2C3B4A59687abcdefghijklmnop";
+    const char				*alphabet1			= "FBZDWAGHrJTLMNOPpRSKUVEXYChImkwQ";
+    const char				*alphabet2			= "F0E1D2C3B4A59687abcdefghijklmnop";
 
-    char				*challenge_lookup	= "qzec2tb3um1olpar8whx4dfgijknsvy5";
-    char				*operand_lookup		= "+|&%/*^-";
-    char				*delimit_lookup		= ",;";
+    const char				*challenge_lookup	= "qzec2tb3um1olpar8whx4dfgijknsvy5";
+    const char				*operand_lookup		= "+|&%/*^-";
+    const char				*delimit_lookup		= ",;";
 
     char				*password_hash		= (char *)malloc(25);
     char				*crypt_hash		    = (char *)malloc(25);
@@ -996,7 +996,7 @@ void YahooClient::process_auth(const char *method, const char *seed, const char 
 
     magic_ptr = (unsigned char*)seed;
 
-    while (*magic_ptr != (int)NULL) {
+    while (*magic_ptr != 0) {
         char   *loc;
 
         /* Ignore parentheses.
