ab02207e0cf3fe61f8e4ad8247009162eb9817f2
crissi <crissi@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2006-05-14 16:55:27 +0000

    compile fix
    git-svn-id: file:///.data/svn_convert/svn@6162 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim/plugins/__migrate/migratedlg.cpp
+++ trunk/sim/plugins/__migrate/migratedlg.cpp
@@ -297,7 +297,7 @@ void MigrateDialog::flush()
         output += number(m_uin);
         output += "\n";
         if (!m_passwd.empty()){
-            m_passwd = string(unquoteString(m_passwd.c_str()));
+	    m_passwd = unquoteString(m_passwd.c_str()).latin1();
             unsigned char xor_table[] =
                 {
                     0xf3, 0x26, 0x81, 0xc4, 0x39, 0x86, 0xdb, 0x92,
