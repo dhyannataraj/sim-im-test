c6f2cdde63cafe3cf1c144cdc93697c0da5a7130
chehrlic <chehrlic@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2006-07-11 19:23:53 +0000

    very initial cmake support - sim exec and lib compiles on win32, also _core lib you'll need cmake 2.4.2
    git-svn-id: file:///.data/svn_convert/svn@6569 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ trunk/cmake/Qt3Uic.cmake
@@ -0,0 +1,38 @@
+# a modified version of KDE3_ADD_UI_FILES from Alexander Neundorf
+
+MACRO(QT3_ADD_UI_FILES _sources )
+   FOREACH (_current_FILE ${ARGN})
+
+      GET_FILENAME_COMPONENT(_tmp_FILE ${_current_FILE} ABSOLUTE)
+
+      GET_FILENAME_COMPONENT(_basename ${_tmp_FILE} NAME_WE)
+      SET(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
+      SET(_src ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
+      SET(_moc ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.moc.cpp)
+
+      ADD_CUSTOM_COMMAND(OUTPUT ${_header}
+         COMMAND ${QT_UIC_EXECUTABLE}
+         ARGS  -nounload -o ${_header} ${CMAKE_CURRENT_SOURCE_DIR}/${_current_FILE}
+         DEPENDS ${_tmp_FILE}
+      )
+
+      ADD_CUSTOM_COMMAND(OUTPUT ${_src}
+         COMMAND uic
+         ARGS
+         -nounload
+         -impl ${_header}
+         ${_tmp_FILE}
+         -o ${_src}
+         DEPENDS ${_header}
+      )
+
+      ADD_CUSTOM_COMMAND(OUTPUT ${_moc}
+         COMMAND ${QT_MOC_EXECUTABLE}
+         ARGS ${_header} -o ${_moc}
+         DEPENDS ${_header}
+      )
+
+      SET(${_sources} ${${_sources}} ${_src} ${_moc} )
+
+   ENDFOREACH (_current_FILE)
+ENDMACRO(QT3_ADD_UI_FILES)
---
+++ trunk/config.h.cmake
@@ -0,0 +1,22 @@
+// version
+#define PACKAGE "SIM"
+#define VERSION "0.9.5"
+
+// header
+#cmakedefine HAVE_INTTYPES_H 1
+#cmakedefine HAVE_STDLIB_H 1
+#cmakedefine HAVE_STDDEF_H 1
+#cmakedefine HAVE_STDINT_H 1
+#cmakedefine HAVE_STRING_H 1
+#cmakedefine HAVE_SYS_TYPES_H 1
+#cmakedefine HAVE_UNISTD_H 1
+
+// symbols
+#cmakedefine HAVE_STRNCASECMP 1
+
+// functions
+#cmakedefine HAVE_MMAP 1
+#cmakedefine HAVE_MUNMAP 1
+
+// libraries
+#cmakedefine USE_OPENSSL 1
