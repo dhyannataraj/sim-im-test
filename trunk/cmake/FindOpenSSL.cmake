1ef95643ee1aec5d09a610e599c47d59595e4cfd
chehrlic <chehrlic@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2006-07-14 06:59:11 +0000

    On win32 now OpenSSL 0.9.8b is needed
    git-svn-id: file:///.data/svn_convert/svn@6602 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/cmake/FindOpenSSL.cmake
+++ trunk/cmake/FindOpenSSL.cmake
@@ -13,20 +13,60 @@ if (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
 else (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
 
   FIND_PATH(OPENSSL_INCLUDE_DIR openssl/ssl.h
+     PATHS
      /usr/include/
      /usr/local/include/
   )
-  
-  FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssl ssleay32
-     PATHS
-     /usr/lib
-     /usr/local/lib
-  )
-  
+
+  IF(WIN32)
+    IF(MSVC)
+      # /MD and /MDd are the standard values - if somone wants to use
+      # others, the libnames have to change here too
+      SET(dgb_ext "MDd" INTERNAL)
+      SET(rel_ext "MD" INTERNAL)
+
+      FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssleay32${dgb_ext}
+         PATHS
+      )
+      SET(SSL_EAY_DEBUG ${OPENSSL_LIBRARIES})
+      FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssleay32${rel_ext}
+         PATHS
+      )
+      SET(SSL_EAY_RELEASE ${OPENSSL_LIBRARIES})
+
+      IF(MSVC_IDE)
+        IF(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
+            SET(OPENSSL_LIBRARIES optimized ${SSL_EAY_RELEASE} debug ${SSL_EAY_DEBUG})
+        ELSE(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
+          MESSAGE(FATAL_ERROR "Could not find the debug and release version of openssl")
+        ENDIF(SSL_EAY_DEBUG AND SSL_EAY_RELEASE)
+      ELSE(MSVC_IDE)
+        STRING(TOLOWER ${CMAKE_BUILD_TYPE} CMAKE_BUILD_TYPE_TOLOWER)
+        IF(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+          SET(OPENSSL_LIBRARIES ${SSL_EAY_DEBUG} INTERNAL FILEPATH)
+        ELSE(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+          SET(OPENSSL_LIBRARIES ${SSL_EAY_RELEASE} INTERNAL FILEPATH)
+        ENDIF(CMAKE_BUILD_TYPE_TOLOWER MATCHES debug)
+      ENDIF(MSVC_IDE)
+    ELSE(MSVC)
+      FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssl ssleay32
+         PATHS
+         /usr/lib
+         /usr/local/lib
+      )
+    ENDIF(MSVC)
+  ELSE(WIN32)
+    FIND_LIBRARY(OPENSSL_LIBRARIES NAMES ssl ssleay32
+       PATHS
+       /usr/lib
+       /usr/local/lib
+    )
+  ENDIF(WIN32)
+
   if (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
      set(OPENSSL_FOUND TRUE)
   endif (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
-  
+
   if (OPENSSL_FOUND)
      if (NOT OpenSSL_FIND_QUIETLY)
         message(STATUS "Found OpenSSL: ${OPENSSL_LIBRARIES}")
@@ -36,7 +76,7 @@ else (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
         message(FATAL_ERROR "Could NOT find OpenSSL")
      endif (OpenSSL_FIND_REQUIRED)
   endif (OPENSSL_FOUND)
-  
+
   MARK_AS_ADVANCED(OPENSSL_INCLUDE_DIR OPENSSL_LIBRARIES)
-  
+
 endif (OPENSSL_INCLUDE_DIR AND OPENSSL_LIBRARIES)
