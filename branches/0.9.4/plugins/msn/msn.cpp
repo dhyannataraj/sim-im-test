cc17ab21bbf1fce47a337d33ef4b544fd8d516d4
wrar <wrar@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2006-08-04 05:12:27 +0000

    update password recovery url for msn, add one for LJ, merge from trunk 870:872
    git-svn-id: file:///.data/svn_convert/svn@6729 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- branches/0.9.4/plugins/msn/msn.cpp
+++ branches/0.9.4/plugins/msn/msn.cpp
@@ -59,7 +59,7 @@ static CommandDef msn_descr =
         I18N_NOOP("MSN"),
         "MSN_online",
         "MSN_invisible",
-        I18N_NOOP("http://help.microsoft.com/!data/en_us/data/messengerv47_xp.its51/$content$/xptroublesignin.htm"),
+        I18N_NOOP("https://accountservices.passport.net/uiresetpw.srf?lc=1033"),
         0,
         0,
         0,
