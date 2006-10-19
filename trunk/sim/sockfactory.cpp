941cdcfdc7329f6f9a085df3c7b018227ec66658
noragen <noragen@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2006-10-19 13:56:49 +0000

    revert 1394, which does not work for win32
    git-svn-id: file:///.data/svn_convert/svn@7068 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim/sockfactory.cpp
+++ trunk/sim/sockfactory.cpp
@@ -40,15 +40,7 @@
 #include <qsocketdevice.h>
 #include <qsocketnotifier.h>
 #include <qtimer.h>
-
-#ifndef WIN32
-// name resolving
-#include <netdb.h> 
-#include <arpa/inet.h>
 #include <qdns.h>
-#else
-#include <qdns.h>
-#endif
 
 #ifndef INADDR_NONE
 #define INADDR_NONE     0xFFFFFFFF
@@ -126,17 +118,7 @@ unsigned long SIMResolver::addr()
 {
     if (dns->addresses().isEmpty())
         return INADDR_NONE;
-#ifdef WIN32
-//    return htonl(dns->addresses().first().isIPv4Address());
-   
-#else
-    // crissi
-    struct hostent * server_entry;
-    if ( ( server_entry = gethostbyname( dns->label().ascii() ) ) == NULL ) {
-	printf( "gethostbyname failed\n" );
-    } else
-    return inet_addr(inet_ntoa(*( struct in_addr* ) server_entry->h_addr_list[ 0 ] ));
-#endif
+    return htonl(dns->addresses().first().ip4Addr());
 }
 
 QString SIMResolver::host() const
