19f889842194846bb03840cb2e263045efc02673
chehrlic <chehrlic@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2006-11-01 18:15:15 +0000

    fix restoring font sizes
    git-svn-id: file:///.data/svn_convert/svn@7165 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim/fontedit.cpp
+++ trunk/sim/fontedit.cpp
@@ -170,17 +170,17 @@ QFont FontEdit::str2font(const QString &str, const QFont &def)
             weight = QFont::Black;
             continue;
         }
-        int p = s.find(QRegExp("[0-9]+ *pt"));
+        int p = s.find(QRegExp(" pt.$"));
         if (p >= 0){
-            s = s.mid(p);
+            s = s.left(p);
             int size = s.toInt();
             if (size > 0)
                 f.setPointSize(size);
             continue;
         }
-        p = s.find(QRegExp("[0-9]+ *pix"));
+        p = s.find(QRegExp(" pix.$"));
         if (p >= 0){
-            s = s.mid(p);
+            s = s.left(p);
             int size = s.toInt();
             if (size > 0)
                 f.setPixelSize(size);
