4e8bbc7b49aab9eb9ce0891c22dac0ea0c04e88c
wrar <wrar@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2006-11-25 13:57:11 +0000

    remove unneeded include
    git-svn-id: file:///.data/svn_convert/svn@7280 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim/qkeybutton.cpp
+++ trunk/sim/qkeybutton.cpp
@@ -21,10 +21,6 @@
 #include <qcursor.h>
 #include <qstringlist.h>
 
-#ifdef USE_KDE
-#include <kglobalaccel.h>
-#endif
-
 QKeyButton::QKeyButton(QWidget *parent, const char *name)
         :	QPushButton( parent, name )
 {
