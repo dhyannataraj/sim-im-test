85571763990bd5a4f46fe0b10127be6c93ca2c38
zowers <zowers@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2007-01-31 00:55:43 +0000

    Script to recursively set on . a fine-tuned svn:ignore property.
    git-svn-id: file:///.data/svn_convert/svn@7503 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

---
+++ branches/0.9.4/svn-ignore.sh
@@ -0,0 +1,46 @@
+COMMON_IGNORE=\
+'
+Makefile
+Makefile.in
+configure.in
+configure
+configure.files
+config.h.in
+config.h
+config.status
+libtool
+*.log
+*.kdevelop
+*.kdevelop.pcs
+*.kdevses
+build-stamp*
+stamp-h.in
+install-stamp-sim-data
+make-common-stamp
+stamp-h1
+svn-commit.tmp
+autom4te.cache
+acinclude.m4
+aclocal.m4
+*.moc
+*base.cpp
+*base.cc
+*base.h
+.libs
+.deps
+*.jisp
+Doxyfile
+'
+
+svn propset -R svn:ignore "$COMMON_IGNORE" .
+
+svn propset -R svn:ignore "$COMMON_IGNORE"\
+'html.cc
+sim' \
+    sim
+
+svn propset -R svn:ignore "$COMMON_IGNORE"'rtf.cc' plugins/icq
+svn propset -R svn:ignore "$COMMON_IGNORE"'parseurl.cc' plugins/navigate
+svn propset -R svn:ignore "$COMMON_IGNORE"'simctrl' plugins/remote
+
+svn propset -R svn:ignore "$COMMON_IGNORE"'*.gmo' po
