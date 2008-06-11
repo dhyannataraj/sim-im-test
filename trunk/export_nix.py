735d26e26fd2704ae84410fb6bd33ab2b95867ea
zowers <zowers@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2008-06-11 10:54:00 +0000

    use --gpl option to export only GPL-compatible files license of file is read from <file>.license license of dir  is read from <dir>/license
    git-svn-id: file:///.data/svn_convert/svn@7883 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/export_nix.py
+++ trunk/export_nix.py
@@ -8,23 +8,55 @@ import sys
 import subprocess
 import shutil
 import fnmatch
+from optparse import OptionParser
 
-target_dir = sys.argv[1]
 PATTERNS = [ "*.vcproj", "*.sln", "install_win32", "win32" ]
+GPL_COMPATIBLE_LICENSES = [ "GPL", "LGPL", "Public Domain", "MPL", "PPL", "MIT", "BSD" ]
+
+parser = OptionParser()
+parser.add_option("-g", "--gpl",
+                  action="store_true", dest="gpl",
+                  help="export only GPL files (mostly icons)")
+parser.set_defaults( gpl = False ) # should be True 
+(options, args) = parser.parse_args()
+target_dir = args[0]
 
 print "Exporting to target directory '%s'" % target_dir
 subprocess.check_call( ["svn", "export", os.curdir, target_dir ] )
 
+def remove_path( path, message ):
+    if os.path.isdir( relname ):
+        print "[%s] Deleting dir '%s'" % ( message, relname )
+        shutil.rmtree( relname )
+    else:
+        print "[%s] Deleting '%s'" % ( message, relname )
+        os.remove( relname )
+
+def filter_path( path ):
+    if not options.gpl:
+        return True
+    else:
+        license_path = None
+        if os.path.exists( path + ".license" ):
+            license_path = path + ".license"
+        elif os.path.exists( os.path.join( path, "license" ) ):
+            license_path = os.path.join( path, "license" )
+        if not license_path:
+            return True
+        else:
+            license_line = open( license_path ).readlines()[0]
+            if license_line in GPL_COMPATIBLE_LICENSES:
+                return True
+            return False
+
 for root, dirs, files in os.walk( target_dir ):
-    for pattern in PATTERNS:
-        for relname in [ os.path.join( root, name ) for name in
-                         fnmatch.filter( dirs + files, pattern ) ]:
-            if os.path.isdir( relname ):
-                print "Deleting dir '%s'" % relname
-                shutil.rmtree( relname )
-            else:
-                print "Deleting '%s'" % relname
-                os.remove( relname )
+    for relname in [ os.path.join( root, name ) for name in dirs + files ]:
+        for pattern in PATTERNS:
+            if not fnmatch.fnmatch( os.path.basename( relname ), pattern ):
+                continue
+            remove_path( relname, "pattern" )
+        if not filter_path( relname ):
+            remove_path( relname, "license" )
 
 print "Done exporting to target directory '%s'" % target_dir
 
@@ -36,3 +68,4 @@ archivepath = os.path.join( archivedir, archivename )
 print "Packing '%s' to '%s'" % ( target_dir, archivepath )
 subprocess.check_call( ["tar", "--bzip2", "-c", "-f", archivename, basename ], cwd = archivedir )
 print "Done"
+
