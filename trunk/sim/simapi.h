48e45fe3600a0a47df89b9e4bd01e4800ebf792e
noragen <noragen@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2008-10-27 15:08:50 +0000

    * formating++
    git-svn-id: file:///.data/svn_convert/svn@7953 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- trunk/sim/simapi.h
+++ trunk/sim/simapi.h
@@ -11,69 +11,69 @@
 #define _SIMAPI_H
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+	#include "config.h"
 #endif
 
 #ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
+	#include <sys/types.h>
 #endif
 #ifdef HAVE_INTTYPES_H
-#include <inttypes.h>
+	#include <inttypes.h>
 #else
-#ifdef HAVE_STDINT_H
-#include <stdint.h>
-#endif
+	#ifdef HAVE_STDINT_H
+		#include <stdint.h>
+	#endif
 #endif
 #ifdef HAVE_UNISTD_H
-#include <unistd.h>
+	#include <unistd.h>
 #endif
 
 // for Q_CC_foo - macros
 #include <qglobal.h>
 
 #ifdef Q_CC_MSVC
-// "conditional expression is constant" (W4)
-# pragma warning(disable: 4127)
-// "'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'" (W1)
-# pragma warning(disable: 4251)
-// "function' : function not inlined" (W4)4786
-# pragma warning(disable: 4710)
+	// "conditional expression is constant" (W4)
+	# pragma warning(disable: 4127)
+	// "'identifier' : class 'type' needs to have dll-interface to be used by clients of class 'type2'" (W1)
+	# pragma warning(disable: 4251)
+	// "function' : function not inlined" (W4)4786
+	# pragma warning(disable: 4710)
 #endif
 
 #ifdef HAVE_GCC_VISIBILITY
-# define SIM_EXPORT __attribute__ ((visibility("default")))
-# define SIM_IMPORT __attribute__ ((visibility("default")))
-# define EXPORT_PROC extern "C" __attribute__ ((visibility("default")))
+	# define SIM_EXPORT __attribute__ ((visibility("default")))
+	# define SIM_IMPORT __attribute__ ((visibility("default")))
+	# define EXPORT_PROC extern "C" __attribute__ ((visibility("default")))
 #elif defined(Q_OS_WIN)
-# define SIM_EXPORT __declspec(dllexport)
-# define SIM_IMPORT __declspec(dllimport)
-# define EXPORT_PROC extern "C" __declspec(dllexport)
+	# define SIM_EXPORT __declspec(dllexport)
+	# define SIM_IMPORT __declspec(dllimport)
+	# define EXPORT_PROC extern "C" __declspec(dllexport)
 #else
-# define SIM_EXPORT
-# define SIM_IMPORT
-#ifdef __OS2__
-# define EXPORT_PROC extern "C" _System
-#else
-# define EXPORT_PROC extern "C"
-#endif
+	# define SIM_EXPORT
+	# define SIM_IMPORT
+	#ifdef __OS2__
+		# define EXPORT_PROC extern "C" _System
+	#else
+		# define EXPORT_PROC extern "C"
+	#endif
 #endif
 
 #ifdef SIMAPI_EXPORTS   // should be set when simapi-lib is build - please add a check for configure
-# define EXPORT SIM_EXPORT
+	# define EXPORT SIM_EXPORT
 #else
-# define EXPORT SIM_IMPORT
+	# define EXPORT SIM_IMPORT
 #endif
 
 #ifdef Q_CC_MSVC
-# define DEPRECATED __declspec(deprecated)
+	# define DEPRECATED __declspec(deprecated)
 #elif defined Q_CC_GNU
-# define DEPRECATED __attribute__ ((deprecated))
+	# define DEPRECATED __attribute__ ((deprecated))
 #else
-# define DEPRECATED
+	# define DEPRECATED
 #endif
 
 #ifndef COPY_RESTRICTED
-# define COPY_RESTRICTED(A) \
+	# define COPY_RESTRICTED(A) \
     private: \
         A(const A&); \
         A &operator = (const A&);
@@ -83,9 +83,9 @@
 	#include <map>
 	#include <string>
 	#include <vector>
-#ifndef __OS2__	
- 	#include <xutility>
-#endif
+	#ifndef __OS2__	
+ 		#include <xutility>
+	#endif
 #else
 	#include <memory>
 	#include <string.h>
