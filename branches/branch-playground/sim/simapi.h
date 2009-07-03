b304e2009ec40228258a262996db466942f668fb
chehrlic <chehrlic@f7a67266-ba00-4dfb-90a2-3eec4ba07b7d>
2009-07-03 17:29:14 +0000

    forgot simapi.h
    git-svn-id: file:///.data/svn_convert/svn@8446 f7a67266-ba00-4dfb-90a2-3eec4ba07b7d

--- branches/branch-playground/sim/simapi.h
+++ branches/branch-playground/sim/simapi.h
@@ -13,20 +13,10 @@
 #ifdef HAVE_CONFIG_H
 	#include "config.h"
 #endif
-
-#ifdef HAVE_SYS_TYPES_H
-	#include <sys/types.h>
-#endif
-#ifdef HAVE_INTTYPES_H
-	#include <inttypes.h>
-#else
-	#ifdef HAVE_STDINT_H
-		#include <stdint.h>
-	#endif
-#endif
-#ifdef HAVE_UNISTD_H
-	#include <unistd.h>
-#endif
+// FIXME: remove me!
+#include <list>
+#include <map>
+#include <vector>
 
 // for Q_CC_foo - macros
 #include <qglobal.h>
@@ -42,37 +32,22 @@
 	# pragma warning(disable: 4512) //Attention should perhaps not be pragma'ed
 #endif
 
-#ifdef HAVE_GCC_VISIBILITY
-	# define SIM_EXPORT __attribute__ ((visibility("default")))
-	# define SIM_IMPORT __attribute__ ((visibility("default")))
-	# define EXPORT_PROC extern "C" __attribute__ ((visibility("default")))
-#elif defined(Q_OS_WIN)
-	# define SIM_EXPORT __declspec(dllexport)
-	# define SIM_IMPORT __declspec(dllimport)
-	# define EXPORT_PROC extern "C" __declspec(dllexport)
+#define SIM_EXPORT Q_DECL_EXPORT
+#define SIM_IMPORT Q_DECL_IMPORT
+
+#ifdef SIMAPI_EXPORTS
+        # define EXPORT SIM_EXPORT
 #else
-	# define SIM_EXPORT
-	# define SIM_IMPORT
-	#ifdef __OS2__
-		# define EXPORT_PROC extern "C" _System
-	#else
-		# define EXPORT_PROC extern "C"
-	#endif
+        # define EXPORT SIM_IMPORT
 #endif
 
-#ifdef SIMAPI_EXPORTS   // should be set when simapi-lib is build - please add a check for configure
-	# define EXPORT SIM_EXPORT
+#ifdef __OS2__
+        # define EXPORT_PROC extern "C" _System
 #else
-	# define EXPORT SIM_IMPORT
+        # define EXPORT_PROC extern "C" SIM_EXPORT
 #endif
 
-#ifdef Q_CC_MSVC
-	# define DEPRECATED __declspec(deprecated)
-#elif defined Q_CC_GNU
-	# define DEPRECATED __attribute__ ((deprecated))
-#else
-	# define DEPRECATED
-#endif
+# define DEPRECATED Q_DECL_DEPRECATED
 
 #ifndef COPY_RESTRICTED
 	# define COPY_RESTRICTED(A) \
@@ -80,19 +55,5 @@
         A(const A&); \
         A &operator = (const A&);
 #endif
-#if defined( WIN32 ) || defined( __OS2__ )
-	#include <list>
-	#include <map>
-	#include <string>
-	#include <vector>
-	#ifdef HAVE_XUTILITY
- 		#include <xutility>
-	#endif
-#else
-	#include <memory>
-	#include <string.h>
-	#include <vector>
-	#include <stdlib.h>
-#endif
-	
+
 #endif // _SIMAPI_H
