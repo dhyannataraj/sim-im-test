1d644516f7166c4a6b564488b0e3da30039eff60
noragen <none@none>
2010-11-20 15:22:51 +0100

    * fixes in standardclientmanager and some small other things

--- plugins/icq/icqclient.cpp
+++ plugins/icq/icqclient.cpp
@@ -265,12 +265,12 @@ ICQClient::~ICQClient()
     delete clientPersistentData;
 }
 
-void ICQClient::initialize(bool /*bAIM*/)
+void ICQClient::initialize(bool bAIM)
 {
     initDefaultStates();
     m_currentStatus = getDefaultStatus("offline");
     initSnacHandlers();
-    //m_bAIM = bAIM;
+    m_bAIM = bAIM;
 
 //    clientPersistentData->owner.setDCcookie(rand());
 
@@ -829,14 +829,13 @@ void ICQClient::setMediaSense(bool b)
 
 QString ICQClient::name()
 {
-//    if(!m_name.isEmpty())
-//        return m_name;
-//    if (m_bAIM) {
-//        m_name = "AIM." + clientPersistentData->owner.getScreen();
-//    }
-//    else {
-//        m_name = "ICQ." + QString::number(clientPersistentData->owner.getUin());
-//    }
+	if(!m_name.isEmpty())
+        return m_name;
+	if (m_bAIM) 
+		m_name = "AIM." + clientPersistentData->owner.getScreen();
+	else
+		m_name = "ICQ." + QString::number(clientPersistentData->owner.getUin());
+	
     return m_name;
 }
 
