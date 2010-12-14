320e4860aa1cbce6104b83c4653b719733b35111
noragen <none@none>
2010-12-14 03:59:01 +0100

    * Merge from trunk, fix of Browse Directory - Crash after Filetransfer

--- plugins/navigate/navigate.cpp
+++ plugins/navigate/navigate.cpp
@@ -435,18 +435,18 @@ bool NavigatePlugin::processEvent(Event *e)
                 DDEconversation conv(server, topic);
                 if (conv.Execute(action))
                     bExec = true;
-            }
-        }*/
-		if (proto=="file") 
-		{
-			QString l_url(url.right(url.length()-strlen("file:")));
-			int len = strlen(l_url.ascii())+1;
-			wchar_t *path = new wchar_t[len];
-			memset(path,0,len);
-			MultiByteToWideChar(  CP_ACP, NULL,l_url.ascii(), -1, path,len );
-			ShellExecute( NULL, NULL, path, NULL, NULL, SW_SHOWNORMAL);
-			bExec = true;
-		}
+            }*/
+			if (proto=="file") 
+			{
+				QString l_url(url.right(url.length()-strlen("file:")));
+				int len = strlen(l_url.ascii())+1;
+				wchar_t *path = new wchar_t[len];
+				memset(path,0,len);
+				MultiByteToWideChar(  CP_ACP, NULL,l_url.ascii(), -1, path,len );
+				ShellExecute( NULL, NULL, path, NULL, NULL, SW_SHOWNORMAL);
+				bExec = true;
+			}
+        }
         if (!bExec)
         {
             if (proto == "file")
