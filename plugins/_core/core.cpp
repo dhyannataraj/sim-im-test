545cf7f9b7bf8a918e1be56df45c4a7a8bb859b5
noragen <none@none>
2011-07-15 03:25:04 +0200

    * Test-Project fixes

--- plugins/_core/core.cpp
+++ plugins/_core/core.cpp
@@ -18,12 +18,12 @@ email                : vovan@shutoff.ru
 #include "simapi.h"
 
 #ifdef WIN32
-#include <windows.h>
-#include <io.h>
+    #include <windows.h>
+    #include <io.h>
 #else
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+    #include <fcntl.h>
+    #include <sys/types.h>
+    #include <sys/stat.h>
 #endif
 
 #include <QSettings>
